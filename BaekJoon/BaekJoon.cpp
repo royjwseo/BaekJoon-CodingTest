c2937febd1b0a8190a7291e6d3a9b779c4f02972
royjwseo <royjwseo@gmail.com>
2024-10-10 19:17:26 +0900

    2292

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,28 +10,22 @@ using namespace std;
 ================= 2024-10-10 ================
 - 백준 단계별로 풀어보기 일반 수학 1
 
---2903번--
-첫째 줄에 N이 주어짐 -> N번 거친 후 정사각형들에 걸쳐 
-중복 점은 단일 저장 가정한 후 저장해야하는 점의 개수 출력
+2292번
 
--> 점은 가로, 세로 증가량이 2의 제곱으로 이루어짐을 확인
--> 2->3->5->9->17->33...
 */
 
 
 int main() {
 
-	int TestCase{};
-	cin >> TestCase;
-
-	int PointNum = 2;
-
-	for (int i = 0; i < TestCase; ++i) {
-		PointNum += pow(2, i);
+	int N{};
+	cin >> N;
+	int cnt{};
+	int imsi{};
+	while (1) {
+		N = N - 6 * cnt++;
+		if (N <= 1)break;
 	}
-	cout << int(pow(PointNum, 2));
-	
-	
+	cout << cnt;
 
 }
 
