f2fa513bb705d716223c299da576d420a66d87c6
royjwseo <royjwseo@gmail.com>
2024-10-12 19:16:51 +0900

    11659번 구간합 1차원

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -7,36 +7,34 @@
 using namespace std;
 
 /*
-================= 2024-10-10 ================
-- 백준 단계별로 풀어보기 일반 수학 1
-2869번
-
-달팽이-> V미터 높이 나무 막대 올라감
-낮엔 A미터, 밤엔 B미터 미끄러짐, 정상에 도달시 미끄러지지X
-나무막대 며칠걸리는지.
-
---해결 방안--
-
+================= 2024-10-12 ================
+- 백준 11659 구간 합 구하기
+시간 제한 1초 (1억 연산)
+1<=N<=10만
+1<=M<=10만
+알고리즘 없이 단순히 구간 합 계산시 최악의 경우 N*M-> 10억 연산 
+합배열 이용하여 O(N+M)으로 줄이기
 */
 
 int main() {
-	int A{}, B{}, V{};
-	cin >> A >> B >> V;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-	int total_meter{};
+	int N,M;
+	cin >> N >> M;
 
-	int one_Day_Up = A - B;
-	int day_Cnt{};
-	
+	vector<int> added_numbers(N+1, 0);
+	int input{};
+	for (int i = 1; i <= N; ++i) {
+		cin >> input;
+		added_numbers[i] = added_numbers[i-1] + input;
+	}
 
-	if (V - A > one_Day_Up) {
-		day_Cnt += (V-A) / one_Day_Up+1;
-
-	}	
-	else
-		day_Cnt = V / one_Day_Up + 1;
-	
-	cout << day_Cnt;
+	int x, y;
+	for (int i = 0; i < M; ++i) {
+		cin >> x >> y;
+		cout << added_numbers[y] - added_numbers[x - 1] << '\n';
+	}
 }
 
 
