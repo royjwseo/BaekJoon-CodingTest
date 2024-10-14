47ca358bc2ce58570636d396421b97a88f5cace1
royjwseo <royjwseo@gmail.com>
2024-10-14 17:42:20 +0900

    15894번 수학은 체육과목 입니다

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -7,11 +7,10 @@
 using namespace std;
 
 /*
-================= 2024-10-12 ================
+================= 2024-10-14 ================
 - 백준 
-3009번 네 번째 점.
+15894번 수학은 체육과목 입니다
 
-세 점을 주고, 축에 평행한 직사각형 만들기 위한 마지막 점
 
 */
 
@@ -22,21 +21,12 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int x[3], y[3];
-	for (int i = 0; i < 3; ++i) {
-		cin >> x[i] >> y[i];
+	long long N;
+	cin >> N;
 
-	}
-	int result_x, result_y;
-	if (x[0] == x[1])result_x = x[2];
-	else if (x[1] == x[2])result_x = x[0];
-	else if (x[2] == x[0])result_x = x[1];
+	cout << 4 * N;
 
-	if (y[0] == y[1])result_y = y[2];
-	else if (y[1] == y[2])result_y = y[0];
-	else if (y[2] == y[0])result_y = y[1];
 
-	cout << result_x << ' ' << result_y;
 }
 
 
