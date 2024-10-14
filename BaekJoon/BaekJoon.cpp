e08325c3dc3007f43f15b4abeb7dae69558791ed
royjwseo <royjwseo@gmail.com>
2024-10-14 17:15:15 +0900

    27323 직사각형

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -9,41 +9,21 @@ using namespace std;
 /*
 ================= 2024-10-12 ================
 - 백준 
-11653 소인수분해
-
-정수 N 소인수분해 결과 오름차순 출력
+27323번 직사각형
 */
 
-bool is_Prime(int num) {
 
-	if (num < 2)return false;
-
-	for (int i = 2; i <= sqrt(num); ++i)
-		if (num % i == 0)return false;
-
-	return true;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+	cout.tie(NULL);
 
-	int N;
-	cin >> N;
 
-	int num;
-	while (1) {
-		if (N == 1)break;
-		for (int i = 2; i <= 10'000'000; ++i) {
-			if (N % i==0) { 
-				cout << i << '\n'; 
-				N /= i;
-				break;
-			}
-		}
-		
-	}
+	int A, B;
+	cin >>A>> B;
 
+	cout << A * B;
 	
 
 }
