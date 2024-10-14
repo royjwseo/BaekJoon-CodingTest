fef94fed954950480ab61bbbd180e5136ea35de6
royjwseo <royjwseo@gmail.com>
2024-10-14 19:07:47 +0900

    24264번 알고리즘 수업 - 알고리즘의 수행 시간 3

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -9,14 +9,15 @@ using namespace std;
 /*
 ================= 2024-10-14 ================
 - 백준 
-24263번 알고리즘 수업 - 알고리즘의 수행 시간 2
+24264번 알고리즘 수업 - 알고리즘의 수행 시간 3
 
 n 1~500'000
 
 MenOfPassion(A[], n) {
 	sum <- 0;
 	for i <- 1 to n
-		sum <- sum + A[i]; # 코드1
+		for j <- 1 to n
+			sum <- sum + A[i] × A[j]; # 코드1
 	return sum;
 }
 */
@@ -30,9 +31,9 @@ int main() {
 
 	int n;
 	cin >> n;
-	//합을 1부터 n까지 더함 O(n)
-	cout << n << '\n';
-	cout << 1;
+	//합을 1부터 n까지 더함 이중 O(n^2)
+	cout << n*n << '\n';
+	cout << 2;
 
 	
 }
