545372f76b4004e1c5688757f33e905e71440201
royjwseo <royjwseo@gmail.com>
2024-10-19 17:48:40 +0900

    3273번 두수의합

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,8 +10,11 @@ using namespace std;
 /*
 ================= 2024-10-19 ================
 - 백준 
-2559 수열 
+3273번 두수의합
 
+N 개의
+각 수 1과 백만 사이
+  
 
 
 
@@ -23,26 +26,41 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int N, K;
-	cin >> N >> K;
-	vector<int>temperatures(N+1,0);
-	int input{};
-	for (int i = 1; i < N + 1; ++i) {
-		cin >> input;
-		temperatures[i] = temperatures[i - 1] + input;
+	int N;
+	cin >> N;
+	vector<int>nums(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> nums[i];
 	}
+	int X;
+	cin >> X;
+
+	sort(nums.begin(), nums.end());
+
 	int left = 0;
-	int right = 0;
-	int leftover;
-	int max = INT_MIN;
-	while (left <= N-K) {
-		int sum= temperatures[left + K] - temperatures[left];
-		if (sum > max) {
-			max = temperatures[left + K] - temperatures[left];
-		}	
-		left++;
-	}
-	cout << max;
+	int right = N - 1;
+	int cnt{};
+
+	
+		left = 0;
+		right = N - 1;
+		while (left <= right) {
+			if (nums[left] + nums[right] > X) {
+				right--;
+			}
+			else if (nums[left] + nums[right] < X) {
+				left++;
+			}
+			else {
+				cnt++;
+				left++;
+				right--;
+			}
+		}
+	
+	cout << cnt;
+
+
 }
 
 
