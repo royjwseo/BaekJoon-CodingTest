5b85f0da4fa51b0a98972434b29c496cf2389286
royjwseo <royjwseo@gmail.com>
2024-10-19 18:28:47 +0900

    1644번 소수의 연속 합

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,16 +10,22 @@ using namespace std;
 /*
 ================= 2024-10-19 ================
 - 백준 
-3273번 두수의합
-
-N 개의
-각 수 1과 백만 사이
-  
-
+1644번 소수의연속합
 
 
 */
-
+bool isPrime(int N) {
+	bool isPrime = true;
+	if (N == 2) return isPrime;
+	for (int i = 2; i < N; ++i) {
+		if (N % i == 0) {
+			isPrime = false;
+			break;
+		}
+	}
+	return isPrime;
+	//O(N)
+}
 
 
 int main() {
@@ -28,38 +34,41 @@ int main() {
 
 	int N;
 	cin >> N;
-	vector<int>nums(N);
-	for (int i = 0; i < N; ++i) {
-		cin >> nums[i];
+
+	vector<int>Primes;
+
+	int sum{};
+	for (int i = 2; i <= N; ++i) {
+		if (isPrime(i)) {
+			Primes.push_back(i);
+		}
 	}
-	int X;
-	cin >> X;
-
-	sort(nums.begin(), nums.end());
-
-	int left = 0;
-	int right = N - 1;
+	int size = Primes.size();
+	vector<int>PrimeSum(size+1,0);
+	for (int i = 1; i <= size; ++i) {
+		PrimeSum[i] = PrimeSum[i - 1] + Primes[i-1];
+	}
+	int left = size-1;
+	int right = size;
 	int cnt{};
 
-	
-		left = 0;
-		right = N - 1;
-		while (left < right) {
-			if (nums[left] + nums[right] > X) {
-
-				right--;
-			}
-			else if (nums[left] + nums[right] < X) {
-				left++;
-			}
-			else {
-				cnt++;
-				left++;
-				right--;
-			}
+	while (left>=0) {
+		int sum = PrimeSum[right] - PrimeSum[left];
+		if (sum < N) {
+			left--;
 		}
-	
+		else if (sum > N) {
+			right--;
+		}
+		else {
+			cnt++;
+			left--;
+			right--;
+		}
+		
+	}
 	cout << cnt;
+	
 
 
 }
