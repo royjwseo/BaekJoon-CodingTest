c1ea0f9ae2a69be158ac90d6f142c60b182b9b98
royjwseo <royjwseo@gmail.com>
2024-10-19 16:56:18 +0900

    2559 수열 , 슬라이딩 윈도우 + 투포인터

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -4,14 +4,15 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
 /*
 ================= 2024-10-19 ================
 - 백준 
-2003 수들의 합
+2559 수열 
 
-0.5초
+
 
 
 */
@@ -22,40 +23,26 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int N, M;
-	cin >> N >> M;
-
-	vector<int>added_nums(N + 1, 0);//1부터사용
-	int input;
-	for (int i = 1; i < N+1; ++i) {
+	int N, K;
+	cin >> N >> K;
+	vector<int>temperatures(N+1,0);
+	int input{};
+	for (int i = 1; i < N + 1; ++i) {
 		cin >> input;
-		added_nums[i] = added_nums[i - 1] + input;
+		temperatures[i] = temperatures[i - 1] + input;
 	}
-
-	int cnt{};
-
 	int left = 0;
-	int right = 1;
-
-		while (right <= N) {
-			int sum = added_nums[right] - added_nums[left];
-			if (sum == M) {
-				cnt++;
-				left++;
-			}
-			else if (sum < M) {
-				right++;
-			}
-			else if (sum > M) {
-				left++;			
-			}
-			if (left == right)right++;
-		}
-
-	
-	cout << cnt;
-	
-
+	int right = 0;
+	int leftover;
+	int max = INT_MIN;
+	while (left <= N-K) {
+		int sum= temperatures[left + K] - temperatures[left];
+		if (sum > max) {
+			max = temperatures[left + K] - temperatures[left];
+		}	
+		left++;
+	}
+	cout << max;
 }
 
 
