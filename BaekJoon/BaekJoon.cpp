a15c9f0f609dc51e21e6effba9cfa0144605f238
royjwseo <royjwseo@gmail.com>
2024-10-21 16:44:19 +0900

    19532번 수학은 비대면강의입니다

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,44 +10,37 @@ using namespace std;
 /*
 ================= 2024-10-21================
 - 백준 
-2798번 분해합
+19532번 수학은 비대면강의입니다
 */
 
 
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	int N;
-	cin >> N;
+    double a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
 
-	int cnt{};
-	int imsi = N;
-	while (imsi != 0) {
-		imsi /= 10;
-		cnt++;
-	}
-	
-	int sum{};
-	int result{};
-	int temp = N;
-	while (temp > 0) {
-		sum = temp;
-		int calc = temp;
-		for (int i = cnt - 1; i >= 0; --i) {
-			int nums = calc / pow(10, i);
-			calc -= nums * pow(10, i);
-			sum += nums;
-		}
+    // 판별식 계산
+    double denominator = a * e - b * d;
 
-		if (sum == N)result = temp;
-		temp--;
-	}
-	cout << result;
-	
+    if (denominator == 0) {
+        // a/e와 b/f가 같은지 확인
+        if (a * f == c * d && b * f == c * e) {
+            cout << "LINE" << endl; // 무한 해
+        }
+        else {
+            cout << "NONE" << endl; // 해 없음
+        }
+    }
+    else {
+        // 유일한 해를 구함
+        int x = (c * e - b * f) / denominator;
+        int y = (a * f - c * d) / denominator;
+        cout << x << " " << y << endl; // 해 출력
+    }
 
-	
+
 }
-
 
