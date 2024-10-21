d5aa145bcb3668fde9bbf5603812b16dcc513eb7
royjwseo <royjwseo@gmail.com>
2024-10-21 15:47:33 +0900

    2798번 블랙잭

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,7 +10,9 @@ using namespace std;
 /*
 ================= 2024-10-21================
 - 백준 
-24313번 알고리즘 수업 - 점근적 표기 1
+2798번 블랙잭
+시간 제한 1초
+입력 최대 100개 중 3개 뽑는 100 x 99 x 98 /6 -> 161700
 */
 
 
@@ -19,14 +21,30 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
+	int N, M;
+	cin >> N >> M;
+	vector<int>nums(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> nums[i];
+	}
 
-	int a1, a0;
-	int c;
-	int n0;
-	cin >> a1 >> a0 >> c >> n0;
+	int sum{};
+	int min = INT_MAX;
+	for (int i = 0; i < N - 2; ++i) {
+		for (int j = i+1; j < N - 1; ++j) {
+			sum = nums[i] + nums[j];
+			for (int k = j + 1; k < N; ++k) {
+				if (nums[k] > M - sum) {
+					continue;
+				}
+				else {
+					if (M - sum - nums[k] <= min)min = M - sum - nums[k];
+				}
+			}
+		}
+	}
+	cout << M - min;
 
-	if (c * n0 >= a1 * n0 + a0&&c>=a1)cout << 1;
-	else cout << 0;
 
 }
 
