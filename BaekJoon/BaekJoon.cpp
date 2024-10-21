9ed23c4007e7a56f5d9969624f6f54830d480d7b
royjwseo <royjwseo@gmail.com>
2024-10-21 16:24:08 +0900

    2798번 분해합

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,9 +10,7 @@ using namespace std;
 /*
 ================= 2024-10-21================
 - 백준 
-2798번 블랙잭
-시간 제한 1초
-입력 최대 100개 중 3개 뽑는 100 x 99 x 98 /6 -> 161700
+2798번 분해합
 */
 
 
@@ -21,31 +19,35 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int N, M;
-	cin >> N >> M;
-	vector<int>nums(N);
-	for (int i = 0; i < N; ++i) {
-		cin >> nums[i];
-	}
+	int N;
+	cin >> N;
 
+	int cnt{};
+	int imsi = N;
+	while (imsi != 0) {
+		imsi /= 10;
+		cnt++;
+	}
+	
 	int sum{};
-	int min = INT_MAX;
-	for (int i = 0; i < N - 2; ++i) {
-		for (int j = i+1; j < N - 1; ++j) {
-			sum = nums[i] + nums[j];
-			for (int k = j + 1; k < N; ++k) {
-				if (nums[k] > M - sum) {
-					continue;
-				}
-				else {
-					if (M - sum - nums[k] <= min)min = M - sum - nums[k];
-				}
-			}
+	int result{};
+	int temp = N;
+	while (temp > 0) {
+		sum = temp;
+		int calc = temp;
+		for (int i = cnt - 1; i >= 0; --i) {
+			int nums = calc / pow(10, i);
+			calc -= nums * pow(10, i);
+			sum += nums;
 		}
+
+		if (sum == N)result = temp;
+		temp--;
 	}
-	cout << M - min;
+	cout << result;
+	
 
-
+	
 }
 
 
