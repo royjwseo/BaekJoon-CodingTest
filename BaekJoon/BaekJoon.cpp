4a2b74db1404e78e3639747bc1e3f984fefdca0d
royjwseo <royjwseo@gmail.com>
2024-10-24 14:40:57 +0900

    1940번 주몽

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,10 +10,9 @@ using namespace std;
 /*
 ================= 2024-10-24================
 - 백준 
-2467번 용액
+1940번
+주몽
 
-산성 1~10억
-알칼리 -1 ~-10억 
 
 */
 
@@ -24,39 +23,33 @@ int main() {
     cin.tie(NULL);
 
     int N;
-    cin >> N;
-    vector<int> liquids(N);
+    int M;
+    cin >> N >> M;
+    vector<int>nums(N);
     for (int i = 0; i < N; ++i) {
-        cin >> liquids[i];
+        cin >> nums[i];
     }
-    
+    sort(nums.begin(), nums.end());
     int left = 0;
-    int right = N - 1;
+    int right = N-1;
 
     int sum{};
-    int result=INT_MAX;
-    int result_left{}, result_right{};
+    int result{};
     while (left < right) {
-        sum = liquids[left] + liquids[right];
-        if (abs(sum) <= result) {
-            result = abs(sum);
-            result_left = left;
-            result_right = right;
-        }
-        if (sum > 0) {
+        sum = nums[left] + nums[right];
+        if (sum > M) {
             right--;
         }
-        else if (sum < 0) {
+        else if(sum<M) {
             left++;
         }
         else {
-            result_left = left;
-            result_right = right;
-        }
-      
-        
+        result++;
+        left++;
+        right--;
+       }
     }
-
-    cout << liquids[result_left] << ' ' << liquids[result_right];
+    cout << result;
 }
+//1 2 3 4 5 7 
 
