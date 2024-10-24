cd344f145ae40035d54a82f88946701e1fffaca3
royjwseo <royjwseo@gmail.com>
2024-10-24 16:04:09 +0900

    2018 세 용액

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,7 +10,7 @@ using namespace std;
 /*
 ================= 2024-10-24================
 - 백준 
-2018번 수들의합 5
+2018번 세 용액
 
 
 */
@@ -24,32 +24,49 @@ int main() {
     int N;
     cin >> N;
 
-    int left = 1;
-    int right = 1;
-
-    int sum=1;
-    int cnt{};
-    while (right <= N) {
-            
-        if (sum > N) {
-            sum -= left;
-            left++;     
-        }
-        else if (sum < N) {
-            right++;
-            sum += right;
-        }
-        else {
-            cnt++;       
-            right++;
-            sum += right;
-            sum -= left;
-            left++;
-        }
+    vector<long long>liquids(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> liquids[i];
     }
-    cout << cnt;
+    sort(liquids.begin(),liquids.end());
+
+    int left = 0;
+    int middle = 1;
+    int right = N - 1;
+    long long sum{};
+    long long max = LONG_MAX;
 
 
+    int result_left{}, result_right{}, result_middle{};
+    while (left < N-2) {
+        middle = left + 1;
+        right =  N-1;
+     
+        while (middle < right) {
+            sum = liquids[left] + liquids[right] + liquids[middle];
+
+            if (abs(sum) < max) {
+                max = abs(sum);
+                result_left = left;
+                result_middle = middle;
+                result_right = right;
+            }
+            if (sum > 0) {
+                right--;
+            }
+            else if (sum < 0) {
+                middle++;
+            }
+            else {
+                break;
+            }
+        }
+        left++;
+
+    }
+    cout << liquids[result_left] << ' ' << liquids[result_middle] << ' ' << liquids[result_right];
+
+   
 }
 
 
