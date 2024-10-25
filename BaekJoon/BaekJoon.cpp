17651c21996757fc129ee0ce0658908b08116ac8
royjwseo <royjwseo@gmail.com>
2024-10-25 18:29:49 +0900

    10773번 제로

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 /*
 ================= 2024-10-25================
-10828번 스택
+10773번 제로
 
 */
 
@@ -20,45 +20,25 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
    
-
-    int N;
-    cin >> N;
-
-    string input;
-    stack<int> my_stack;
-    for (int i = 0; i < N; ++i) {
-        cin >> input;
-        if (input == "push") {
-            int a;
-            cin >> a;
-            my_stack.push(a);
+    int K;
+    cin >> K;
+    vector<int> moneystack;
+    for (int i = 0; i < K; ++i) {
+        int money;
+        cin >> money;
+        if (money == 0) {
+            moneystack.pop_back();
         }
-        else if (input == "top") {
-            if (my_stack.empty()) {
-                cout << "-1\n";
-            }
-            else {
-                cout << my_stack.top() << '\n';
-            }
-            
-        }
-        else if (input == "size") {
-            cout << my_stack.size()<<'\n';
-        }
-        else if (input == "pop") {
-            if (my_stack.empty()) {
-                cout << "-1\n";
-            }
-            else {
-                cout << my_stack.top() << '\n';
-                my_stack.pop();
-            }
-        }
-        else if (input == "empty") {
-            (my_stack.empty()) ? cout << "1\n" : cout << "0\n";
+        else {
+            moneystack.push_back(money);
         }
     }
-   
+
+    int sum{};
+    for (int& a : moneystack) {
+        sum += a;
+    }
+    cout << sum;
  
    
 }
