65a549ef56e631eea10e211cdff806bc995460ed
royjwseo <royjwseo@gmail.com>
2024-10-25 17:23:34 +0900

    9012번 괄호

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -8,64 +8,47 @@
 using namespace std;
 
 /*
-================= 2024-10-24================
-- 백준 
-2473번 세 용액
+================= 2024-10-25================
+9012번 괄호
 
 
 */
 
 
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+   
     int N;
     cin >> N;
 
-    vector<long long>liquids(N);
+
     for (int i = 0; i < N; ++i) {
-        cin >> liquids[i];
-    }
-    sort(liquids.begin(),liquids.end());
+        string sen;
 
-    int left = 0;
-    int middle = 1;
-    int right = N - 1;
-    long long sum{};
-    long long max = LONG_MAX;
-
-
-    int result_left{}, result_right{}, result_middle{};
-    while (left < N-2) {
-        middle = left + 1;
-        right =  N-1;
-     
-        while (middle < right) {
-            sum = liquids[left] + liquids[right] + liquids[middle];
-
-            if (abs(sum) < max) {
-                max = abs(sum);
-                result_left = left;
-                result_middle = middle;
-                result_right = right;
+        int cnt{};
+       bool start = false;
+        cin >> sen;
+        for (char a : sen) {
+            if (a == '(') {
+                start = true;
+                cnt++;
             }
-            if (sum > 0) {
-                right--;
+            else if (a == ')')
+            {
+                cnt--;
+                if (cnt < 0)break;
+                if (cnt == 0)start = false;
             }
-            else if (sum < 0) {
-                middle++;
-            }
-            else {
-                break;
-            }
+       }
+        if (cnt == 0 && !start) {
+            cout << "YES\n";
         }
-        left++;
-
+        else {
+            cout << "NO\n";
+        }
     }
-    cout << liquids[result_left] << ' ' << liquids[result_middle] << ' ' << liquids[result_right];
-
+   
    
 }
 
