dfbe0e3906710162b697575f6a0e8b4de71899c7
royjwseo <royjwseo@gmail.com>
2024-11-04 20:04:59 +0900

    2750 버블 정렬 이용

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -9,8 +9,10 @@
 using namespace std;
 
 /*
-================= 2024-10-28================
-10989번 수 정렬하기 3
+================= 2024-11-04================
+2750번 수 정렬하기1
+
+버블 정렬 구현
 */
 
 
@@ -22,18 +24,21 @@ int main() {
    
     int N;
     cin >> N;
-    int a;
-    array<int, 10001>nums{};
-    for (int i = 0; i < N; ++i) {
-        cin >> a;
-        nums[a]++;
-    }
-    for (int i = 1; i <= 10000; ++i) {
-        if (nums[i] > 0) {
-            for (int j = 0; j < nums[i]; j++)
-                cout << i << '\n';
+    //입력은 1000 이므로 log(n^2)여도 1'000'000 이므로 버블로 해도됨.
+
+    vector<int>nums(N);
+    for (int i = 0; i < N; ++i)cin >> nums[i];
+
+    for (int i = 0; i < N-1; ++i) {
+        for (int j = 0; j < N-i-1; j++) {
+            if (nums[j] > nums[j+1])swap(nums[j], nums[j+1]);
         }
     }
+
+    for (int& a : nums) {
+        cout << a << '\n';
+    }
+
 }
 
 
