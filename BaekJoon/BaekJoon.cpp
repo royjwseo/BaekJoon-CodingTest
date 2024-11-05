6d90f5c3f1fa7732a2d5fb7ed2ba7950eacde336
royjwseo <royjwseo@gmail.com>
2024-11-05 18:07:30 +0900

    pccp 모의고사 1, 1번 외톨이 알파벳

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,40 +10,42 @@
 using namespace std;
 
 /*
-================= 2024-11-04================
-2750번 수 정렬하기
-삽입 정렬 구현
+================= 2024-11-05================
+pccp 모의고사 1번 - 외톨이 알파벳
 */
 
 
+string solution(string input_string) {
+    string answer = "";
+    bool has_answer = false;
+    for (int i = 0; i < input_string.size(); ++i) {
+        bool continuous = true;
+        char target = input_string[i];
+        for (int j = i + 1; j < input_string.size(); ++j) {
+            if (input_string[j] == target && input_string[j - 1] == target)continue;
+            if (input_string[j] == target) {
+                answer.push_back(target);
+                has_answer = true;
+                break;
+            }
+            else {
+                continuous = false;
+            }
+        }
+    }
+    sort(answer.begin(), answer.end());
+    answer.erase(unique(answer.begin(), answer.end()), answer.end());
 
+    if (!has_answer)answer = "N";
+
+    return answer;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
  
-    int N;
-    cin >> N;
-
-    vector<int>nums(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> nums[i];
-    }
-
-    for (int i = 1; i < N; ++i) {
-        int key = nums[i];
-        int j = i - 1;
-        while (j >= 0 && nums[j] > key) {
-            nums[j + 1] = nums[j];
-            j--;
-
-        }
-        nums[j + 1] = key;
-       
-    }
-    for (int& a : nums) {
-        cout << a << '\n';
-    }
+ 
 }
 
 
