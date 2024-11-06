b14442a8c5d65912902b2d0d16ea8970c0d89419
royjwseo <royjwseo@gmail.com>
2024-11-06 19:24:35 +0900

    1715번 카드 정렬하기

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -11,45 +11,47 @@ using namespace std;
 
 /*
 ================= 2024-11-06================
-11286 절댓값 힙
+1715 카드 정렬하기
 
 */
 
-struct abs_compare {
-
-    bool operator()(int a, int b) {
-        if (abs(a) == abs(b))return a>b;
-        else
-        return abs(a) > abs(b);
-    }
-};
 
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
  
-  
     int N;
     cin >> N;
 
-    priority_queue<int, vector<int>, abs_compare>pq;
-
+    priority_queue<int, vector<int>, std::greater<int>>pq;
     for (int i = 0; i < N; ++i) {
-        int input{};
+        int input;
         cin >> input;
-        if (input == 0) {
-            if (pq.empty()) {
-                cout << "0\n";
-            }else {
-                cout << pq.top() << '\n';
-                pq.pop();
-            }
-        }
-        else {
-            pq.push(input);
-        }
+        pq.push(input);
     }
+
+    int sum{};
+    int imsi_sum{};
+    int left, right;
+
+
+    for (int i = 0; i < N - 1; ++i) {
+        imsi_sum = 0;
+        left = 0; right = 0;
+        left = pq.top();
+        pq.pop();
+        if (!pq.empty()) {
+            right = pq.top();
+            pq.pop();
+        }
+        imsi_sum = left + right;
+        pq.push(imsi_sum);
+        sum += left + right;
+    }
+
+  
+    cout << sum;
  
 }
 
