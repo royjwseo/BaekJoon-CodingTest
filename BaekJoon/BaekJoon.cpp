6ed80aa2fb9ab6a85e9c9c6df7a723f947f3cf1c
royjwseo <royjwseo@gmail.com>
2024-11-06 18:38:28 +0900

    1927 최소힙

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,82 +10,11 @@
 using namespace std;
 
 /*
-================= 2024-11-05================
+================= 2024-11-06================
+1927번 최소 힙
 
 */
 
-
-template<class T>
-class PriorityQueue
-{
-public:
-    void push(const T& data) {
-
-        // 우선 힙 구조부터 맞춰준다.
-        _heap.push_back(data); 
-
-        // 대소에 의한 정렬 시작
-        int cur_index = static_cast<int>(_heap.size())-1;
-
-        //루트 노드까지
-        while (cur_index > 0) {
-            int parent_index = (cur_index - 1) / 2;
-            if (_heap[cur_index] > _heap[parent_index]) {
-                swap(_heap[cur_index], _heap[parent_index]);
-                cur_index = parent_index;
-            }
-            else {
-                break;
-            }
-        }
-    }
-    
-    void pop() {
-        _heap[0] = _heap.back();
-        _heap.pop_back();
-
-        int cur_index = 0;
-
-        while (true) {
-            int left = (2 * cur_index) + 1;
-            int right = (2 * cur_index) + 2;
-
-            //리프에 도달한 경우
-            if (left >= (int)_heap.size())
-                break;
-
-            int next = now;
-            //왼쪽과 비교
-            if (_heap[next] < _heap[left]) {
-                next = left;
-            }
-
-            // 둘 중 승자를 오른쪽과 비교 (하지만 오른쪽이 없을수도 있으니 체크해주고)
-            if (right < _heap.size() && _heap[next] < _heap[right]) {
-                next = right;
-            }
-
-            //왼쪽 오른쪽 둘 다 현재 값보다 작으면 종료
-            if (next == now)
-                break;
-
-            ::swap(_heap[now], _heap[next]);
-            now = next;
-        }
-        
-    }
-
-    T& top() {
-        return _heap[0];
-    }
-
-    bool empty() {
-        return _heap.empty();
-    }
-
-private:
-    vector<T> _heap;
-};
 
 
 int main() {
@@ -93,7 +22,25 @@ int main() {
     cin.tie(NULL);
  
   
-    
+    int N;
+    cin >> N;
+
+    priority_queue<int, vector<int>, greater<int>>pq;
+
+    for (int i = 0; i < N; ++i) {
+        int input;
+        cin >> input;
+        if (input == 0) {
+            if (pq.empty())cout << "0\n";
+            else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+        else {
+            pq.push(input);
+        }
+    }
  
 }
 
