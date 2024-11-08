4a12198a08feb20255cb831f0597afd1bb047c11
royjwseo <royjwseo@gmail.com>
2024-11-08 15:29:30 +0900

    1655번 가운데를 말해요

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -11,8 +11,7 @@ using namespace std;
 
 /*
 ================= 2024-11-08================
-2559번 수열
-
+1655번 가운데를 말해요
 */
 
 
@@ -21,30 +20,34 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
  
+    priority_queue<int, vector<int>, greater<int>>min_heap;
+    priority_queue<int>max_heap;
+   
+
     int N;
-    int K;
-    cin >> N >> K;
+    cin >> N;
 
-    vector<int> adds(N + 1);
+    for (int i = 0; i < N; ++i) {
+        int input;
+        cin >> input;
 
-    for (int i = 1; i <= N; ++i) {
-        int a;
-        cin >> a;
-        adds[i] = adds[i - 1] + a;
+        if (max_heap.empty() || input < max_heap.top()) {
+            max_heap.push(input);
+        }
+        else {
+            min_heap.push(input);
+        }
+
+        if (max_heap.size() > min_heap.size()+1) {
+            min_heap.push(max_heap.top());
+            max_heap.pop();
+        }
+        else if (min_heap.size() > max_heap.size()) {
+            max_heap.push(min_heap.top());
+            min_heap.pop();
+        }
+        cout << max_heap.top()<<'\n';
     }
-
-    int max = INT_MIN;
-    int left = 0;
-    int right = left + K;
-    while (right < adds.size()) {
-        
-        int sum = adds[right] - adds[left];
-        if (sum > max)max = sum;
-        left++;
-        right++;
-    }
-    cout << max;
-
 }
 
 
