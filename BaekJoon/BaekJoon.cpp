9c6ed9a9850f481913d17ed480f2581281e9b08a
royjwseo <royjwseo@gmail.com>
2024-11-10 14:57:05 +0900

    1260 DFS와 BFS

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,44 +10,83 @@
 using namespace std;
 
 /*
-================= 2024-11-08================
-1655번 가운데를 말해요
+================= 2024-11-10================
+//1260 DFS와 BFS
 */
+//DFS 방문한 목록
+vector<bool>visited;
+vector<vector<int>>adjacent;
 
+//BFS 발견한 목록
+vector<bool> discovered;
 
+void DFS(int here){
+    visited[here] = true;
+    cout << here << ' ';
+    int size = adjacent[here].size();
+    for (int i = 0; i < size; ++i) {
+        if (!visited[adjacent[here][i]]) {
+            DFS(adjacent[here][i]);
+        }
+       
+    }
+}
+
+void BFS(int here) {
+
+    queue<int> q;
+    q.push(here);
+    discovered[here] = true;
+
+    while (q.empty() == false) {
+        here = q.front();
+        q.pop();
+
+        cout <<  here << ' ';
+
+        int size = adjacent[here].size();
+        for (int i = 0; i < size; ++i) {
+            int there = adjacent[here][i];
+            if (discovered[there])continue;
+
+            q.push(there);
+            discovered[there] = true;
+        }
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
- 
-    priority_queue<int, vector<int>, greater<int>>min_heap;
-    priority_queue<int>max_heap;
-   
 
     int N;
     cin >> N;
+    int M;
+    cin >> M;
+    int V;
+    cin >> V;
 
-    for (int i = 0; i < N; ++i) {
-        int input;
-        cin >> input;
+ 
 
-        if (max_heap.empty() || input < max_heap.top()) {
-            max_heap.push(input);
-        }
-        else {
-            min_heap.push(input);
-        }
-
-        if (max_heap.size() > min_heap.size()+1) {
-            min_heap.push(max_heap.top());
-            max_heap.pop();
-        }
-        else if (min_heap.size() > max_heap.size()) {
-            max_heap.push(min_heap.top());
-            min_heap.pop();
-        }
-        cout << max_heap.top()<<'\n';
+    adjacent.resize(N+1);
+    visited.resize(N + 1, false);
+    discovered.resize(N + 1, false);
+    for (int i = 0; i < M; ++i) {
+        int a, b;
+        cin >> a >> b;
+        adjacent[a].push_back(b);
+        adjacent[b].push_back(a);
     }
+    int size = adjacent.size();
+    for (int i = 0; i < size; ++i) {
+        sort(adjacent[i].begin(), adjacent[i].end());
+    }
+    DFS(V);
+    cout << '\n';
+    BFS(V);
+
+
+  
 }
 
 
