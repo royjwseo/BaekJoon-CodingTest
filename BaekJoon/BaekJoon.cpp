a678153707d2283918838c98c39cf9726df116d0
royjwseo <royjwseo@gmail.com>
2024-11-10 16:07:52 +0900

    2606 바이러스 스택버전DFS

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -11,77 +11,54 @@ using namespace std;
 
 /*
 ================= 2024-11-10================
-//1260 DFS와 BFS
+//2606번 바이러스
 */
-//DFS 방문한 목록
-vector<bool>visited;
-vector<bool>visited_stack;
-vector<vector<int>>adjacent;
 
-
-void DFS(int here){
-    visited[here] = true;
-    cout << here << ' ';
-    int size = adjacent[here].size();
-    for (int i = 0; i < size; ++i) {
-        if (!visited[adjacent[here][i]]) {
-            DFS(adjacent[here][i]);
-        }
-       
-    }
-}
-
-void DFS_STACK(int here) {
+vector<vector<int>> adjacents;
+vector<bool> visited;
+int cnt{};
+void BFS(int here) {
     stack<int> st;
+    
     st.push(here);
-
     while (!st.empty()) {
         int top = st.top();
-        if (!visited_stack[top]) {
-            visited_stack[top] = true;
-            cout << top << ' ';
-        }
-          st.pop();
-
-          int adj = adjacent[top].size();
-          if (adj > 0) {
-              for (int i = 0; i < adj; ++i) {
-                  if (!visited_stack[adjacent[top][i]])
-                      st.push(adjacent[top][i]);
-              }
-          }
-
+    if (!visited[top]) {
+        visited[top] = true;
+        cnt++;
     }
+    st.pop();
     
+        int size = adjacents[here].size();
+        for (int i = 0; i < size; ++i) {
+            if (!visited[adjacents[here][i]]) {
+                st.push(adjacents[here][i]);
+            }
+        }
+    }
 }
-
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int N, M, V;
-    cin >> N >> M >> V;
-
-    adjacent.resize(N + 1);
+    int N;
+    int E;
+    cin >> N >> E;
+    adjacents.resize(N + 1);
     visited.resize(N + 1, false);
-    visited_stack.resize(N + 1, false);
 
-    for (int i = 0; i < M; ++i) {
+    for (int i = 0; i < E; ++i) {
         int a, b;
         cin >> a >> b;
-        adjacent[a].push_back(b);
-        adjacent[b].push_back(a);
-
+        adjacents[a].push_back(b);
+        adjacents[b].push_back(a);
     }
-  
-    DFS(V);
-    cout << '\n';
-    for (int i = 0; i < adjacent.size(); ++i)
-        sort(adjacent[i].begin(), adjacent[i].end(), std::greater<int>());
+   
+    BFS(1);
+    cout << cnt - 1;
 
-    DFS_STACK(V);
-  
+
 }
 
 
