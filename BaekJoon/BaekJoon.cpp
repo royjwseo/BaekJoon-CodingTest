a96670f044a66cf24280029884e5d711b62c245d
royjwseo <royjwseo@gmail.com>
2024-11-14 21:21:47 +0900

    11866번 요세푸스 문제0

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -11,33 +11,51 @@ using namespace std;
 
 /*
 ================= 2024-11-14================
-2164 카드 2
+11866번 요세푸스 문제0
 */
-
-
-
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    queue<int>cards;
     int N;
     cin >> N;
+    vector<int>answer;
+
+    int K;
+    cin >> K;
+
+    queue<int> q;
     for (int i = 1; i <= N; ++i) {
-        cards.push(i); //O(1) ->N번 O(N)
+        q.push(i);
     }
-     
+
     while (1) {
-        if (cards.size() == 1)break;
+        if (q.empty())break;
+        int cnt = K-1;
+        while (cnt > 0) {
+            int front = q.front();
+            q.pop();
+            q.push(front);
+            cnt--;
+        }
+        int front = q.front();
+        answer.push_back(front);
+        q.pop();
 
-        cards.pop();
-        int front = cards.front();
-        cards.pop();
-        cards.push(front);
     }
-
-    cout << cards.front();
+   
+    cout << '<';
+    for (vector<int>::iterator iter = answer.begin(); iter != answer.end(); ++iter) {
+        if (iter == answer.end() - 1) {
+            cout << *iter;
+        }
+        else {
+            cout << *iter << ", ";
+        }
+    }
+    
+    cout << ">\n";
 
 }
 
