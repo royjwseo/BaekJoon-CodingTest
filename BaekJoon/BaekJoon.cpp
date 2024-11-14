7188c3a9c32311084feb61d39cba83fc75b3bae3
royjwseo <royjwseo@gmail.com>
2024-11-14 21:02:52 +0900

    2164 카드2

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 /*
 ================= 2024-11-14================
-18258번 큐 2
+2164 카드 2
 */
 
 
@@ -21,54 +21,23 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    queue<int> q;
-
+    queue<int>cards;
     int N;
     cin >> N;
+    for (int i = 1; i <= N; ++i) {
+        cards.push(i); //O(1) ->N번 O(N)
+    }
+     
+    while (1) {
+        if (cards.size() == 1)break;
 
-    string input;
-    for (int i = 0; i < N; ++i) {
-        cin >> input;
-
-        if (input == "push") {
-            int a;
-            cin >> a;
-            q.push(a);
-        }
-        else if (input == "front") {
-            if (q.empty()) {
-                cout << "-1\n";
-            }
-            else {
-                cout << q.front() << '\n';
-            }
-        }
-        else if (input == "pop") {
-            if (q.empty()) {
-                cout << "-1\n";
-            }
-            else {
-                cout << q.front() << '\n';
-                q.pop();
-            }
-        }
-        else if (input == "size") {
-            cout << q.size() << '\n';
-        }
-        else if (input == "empty") {
-            (q.empty()) ? cout<<"1\n" : cout<<"0\n";
-        }
-        else if (input == "back") {
-            if (q.empty()) {
-                cout << "-1\n";
-            }
-            else {
-                cout << q.back() << '\n';
-            }
-        }
-
+        cards.pop();
+        int front = cards.front();
+        cards.pop();
+        cards.push(front);
     }
 
+    cout << cards.front();
 
 }
 
