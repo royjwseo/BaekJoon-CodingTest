104ae6bde3846ac06b94c6a4ed2fb37adedc8572
royjwseo <royjwseo@gmail.com>
2024-11-16 00:30:40 +0900

    1920 수 찾기 - 이진탐색

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -12,43 +12,59 @@ using namespace std;
 
 /*
 ================= 2024-11-15================
-24511 queuestack
+1920번 수 찾기
+
+//
+1KB -> 1024바이트
+1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
+스택 크기 : 1MB
 */
+
+bool binary_search(int n, vector<int>& nums) {
+    int right = nums.size()-1;
+    int left = 0;
+    int index; 
+    while (left<=right) {
+        index = (right + left) / 2;
+        if (n > nums[index]) {
+            left = index + 1;
+        }
+        else if (n < nums[index]) {
+            right = index-1;
+        }
+        else {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    vector<int> nums;
     int N;
     cin >> N;
-
-    vector<int> which(N);
     for (int i = 0; i < N; ++i) {
-        cin >> which[i];
-    }
-
-    deque<int> q;
-    for (int i = 0; i < N; ++i) {  
         int input;
         cin >> input;
-        if (which[i] == 0) {
-            q.push_front(input);
+        nums.push_back(input);
+    }
+    sort(nums.begin(), nums.end()); //O(nlogn) 100000*16.61 =160만
+    int M;
+    cin >> M;
+    for (int i = 0; i < M; ++i) {
+        int input;
+        cin >> input;
+        if (binary_search(input, nums)) {
+            cout << 1 << '\n';
         }
-
+        else {
+            cout << 0 << '\n';
+        }
     }
-    vector<int>answer;
-    int len;
-    cin >> len;
-    for (int i = 0; i < len; ++i) {
-        int num;
-        cin >> num;
-        q.push_back(num);
-        answer.push_back(q.front());
-        q.pop_front();
-    }
-    for (int& a : answer) {
-        cout << a << ' ';
-    }
+    
 }
 
 
