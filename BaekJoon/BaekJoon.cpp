4d14f206687e60f0c95245c1604a60d8aed298de
royjwseo <royjwseo@gmail.com>
2024-11-17 17:30:53 +0900

    25305번 커트라인

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -11,10 +11,9 @@
 using namespace std;
 
 /*
-================= 2024-11-15================
-28279번 덱 2
+================= 2024-11-17================
+25305번 커트라인
 
-//
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -28,53 +27,17 @@ int main() {
 
     int N;
     cin >> N;
-    deque<int> dq;
+    int K;
+    cin >> K;
+
+    vector<int> grades(N);
     for (int i = 0; i < N; ++i) {
-        int input;
-        cin >> input;
-        switch (input) {
-        case 1:
-            int num;
-            cin >> num;
-            dq.push_front(num);
-            break;
-        case 2:
-            int num2;
-            cin >> num2;
-            dq.push_back(num2);
-            break;
-        case 3:
-            if (!dq.empty()) {
-                cout << dq.front()<<'\n';
-                dq.pop_front();
-            }
-            else {
-                cout << "-1\n";
-            }
-            break;
-        case 4:
-            if (!dq.empty()) {
-                cout << dq.back() << '\n';
-                dq.pop_back();
-            }
-            else {
-                cout << "-1\n";
-            }
-            break; 
-        case 5:
-            cout << dq.size()<<'\n';
-            break; 
-        case 6:
-            (dq.empty()) ? cout << "1\n" :cout<<"0\n";
-            break; 
-        case 7:
-            (dq.empty()) ? cout << "-1\n" : cout << dq.front() << '\n';
-            break;
-        case 8:
-            (dq.empty()) ? cout << "-1\n" : cout << dq.back() << '\n';
-            break;
-        }
+        cin >> grades[i];
     }
+    partial_sort(grades.begin(), grades.begin() + K, grades.end(), [](int a, int  b) {
+        return a > b;
+        });
+    cout << grades[K-1];
     
 }
 
