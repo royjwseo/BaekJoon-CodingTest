20a6ec8584973e608ec9a8d3ddd508e1d51ee10d
royjwseo <royjwseo@gmail.com>
2024-11-17 23:09:07 +0900

    24444번 알고리즘 수업 - 너비 우선 탐색 1

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -12,26 +12,35 @@ using namespace std;
 
 /*
 ================= 2024-11-17================
-24480번 알고리즘 수업 - 깊이 우선 탐색 2
+24444번 알고리즘 수업 - 너비 우선 탐색 1
 
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
-vector<vector<int>>adjacent;
-vector<int> visited;
-int n{};
+
+vector<vector<int>> adjacent;
+vector<int> visit;
+int n = 1;
 
 void BFS(int here) {
-	
-	if (visited[here]==0) {
-		visited[here] = ++n;
-	}
-	int n = adjacent[here].size();
-	for (int i = 0; i < n; ++i) {
+
+	queue<int> q;
+	q.push(here);
+	visit[here] = n++;
+	while (!q.empty()) {
+		int front = q.front();
 		
-		if (visited[adjacent[here][i]]==0)
-			BFS(adjacent[here][i]);
+		q.pop();
+
+		int a = adjacent[front].size();
+		for (int i = 0; i < a; ++i) {
+			if (visit[adjacent[front][i]]==0) {
+				visit[adjacent[front][i]] = n++;
+				q.push(adjacent[front][i]);
+			}
+		}
+
 	}
 
 }
@@ -42,27 +51,27 @@ int main() {
 	
 	int N;
 	int M;
-	cin >> N >> M;
 	int R;
-	cin >> R;
-
-	adjacent.resize(N+1);
-	visited.resize(N+1, 0);
+	cin >> N >> M >> R;
 	
+	adjacent.resize(N+1);
+	visit.resize(N + 1, 0);
 	for (int i = 0; i < M; ++i) {
 		int a, b;
 		cin >> a >> b;
+
 		adjacent[a].push_back(b);
 		adjacent[b].push_back(a);
 	}
 	for (int i = 1; i <= N; ++i) {
-		sort(adjacent[i].begin(), adjacent[i].end(),greater<int>());
-	}
-	BFS(R);
-	for (int i = 1; i <= N; ++i) {
-		cout << visited[i] << '\n';
+		sort(adjacent[i].begin(), adjacent[i].end());
 	}
 
+	BFS(R);
+
+	for (int i = 1; i <= N; ++i) {
+		cout << visit[i]<<'\n';
+	}
 
 	
 
