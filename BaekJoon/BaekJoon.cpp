f7e95f81305532e7dbe0c6029503e1acde39c12c
royjwseo <royjwseo@gmail.com>
2024-11-21 19:58:04 +0900

    25501번 재귀의 귀재

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -12,28 +12,33 @@ using namespace std;
 
 /*
 ================= 2024-11-21================
-27433번 팩토리얼 2
+25501번 재귀의 귀재
 
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
-int fibonacci(int i) {
-
-	if (i <=0)return 0;
-	else if (i == 1)return 1;
-
-	return fibonacci(i-1) + fibonacci(i - 2);
+int recursion(string s,int l,int r,int& cnt) {
+	cnt++;
+	if (l >= r)return 1;
+	else if (s[l] != s[r])return 0;
+	else return recursion(s, l + 1, r - 1,cnt);
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
-	int n;
-	cin >> n;
-	cout<<fibonacci(n);
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; ++i) {
+		string input;
+		cin >> input;
+		int cnt{};
+		cout << recursion(input, 0, input.size() - 1, cnt)<<' '<<cnt;
+
+	}
 	
 }
 
