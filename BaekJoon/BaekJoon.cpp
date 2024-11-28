3d28ebf9ac66ce86f0b46a3d02bb33d4df52356e
royjwseo <royjwseo@gmail.com>
2024-11-28 19:46:30 +0900

    17298번 오큰수

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -30,61 +30,23 @@ int main() {
 	stack<int>st;
 	int N;
 	cin >> N;
-
+	vector<int> vec(N);
 	for (int i = 0; i < N; ++i) {
-		int input;
-		cin >> input;
-		st.push(input);
+		cin >> vec[i];
 	}
-	vector<pair<int, int>>reversed_nums;
-	int max = st.top();
-	int before = -1;
-	bool check = false;
-	int real_max = -1;
-	bool real_max_check = false;
+	int max = -1;
+	vector<int> answer(N,-1);
 	for (int i = 0; i < N; ++i) {
-		int top = st.top();
-		st.pop();
-		if (top > real_max) {
-			real_max = top;
-		}
 		
-		if (!check) {
-			reversed_nums.push_back({ top,max });
+		while (!st.empty() && vec[st.top()] < vec[i]) {
+			answer[st.top()]=(vec[i]);
+			st.pop();
 		}
-		else if (real_max_check) {
-			reversed_nums.push_back({ top,real_max });
-			real_max_check = false;
-		}
-		else {
-			reversed_nums.push_back({ top,top });
-		}
-		
-		if (!st.empty()) {
-			if (st.top() < top) {
-				check = false;
-				max = top;		
-			}
-			else if(st.top()>max) {
-				if (st.top() < real_max) {
-					real_max_check = true;
-				}
-				check = true;			
-			}
-		}
-	
+		st.push(i);
 	}
 
-
-	for (auto it = reversed_nums.rbegin(); it != reversed_nums.rend(); it++) {
-		if (it->first == it->second) {
-			cout << -1;
-		}
-		else {
-			cout << it->second;
-		}
-		if(it!=reversed_nums.rend())
-		cout << ' ';
+	for (auto a : answer) {
+		cout << a << ' ';
 	}
 
 }
