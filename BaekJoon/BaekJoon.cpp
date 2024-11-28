995b8d644f8aaf7e15e2698bfd8bde2c4e8872a1
royjwseo <royjwseo@gmail.com>
2024-11-28 19:22:57 +0900

    17289 오큰수 실패 코드

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 /*
 ================= 2024-11-28================
-14425번 문자열 집합
+17298번 오큰수
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -25,28 +25,67 @@ using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(nullptr);
 
-	int N, M;
-	cin >> N >> M;
-
-	unordered_set<string> set;
+	stack<int>st;
+	int N;
+	cin >> N;
 
 	for (int i = 0; i < N; ++i) {
-		string input;
+		int input;
 		cin >> input;
-		set.insert(input);
+		st.push(input);
 	}
-	int cnt{};
-	for (int i = 0; i < M; ++i) {
-		string find_str;
-		cin >> find_str;
-
-		if (set.find(find_str) != set.end()) {
-			cnt++;
+	vector<pair<int, int>>reversed_nums;
+	int max = st.top();
+	int before = -1;
+	bool check = false;
+	int real_max = -1;
+	bool real_max_check = false;
+	for (int i = 0; i < N; ++i) {
+		int top = st.top();
+		st.pop();
+		if (top > real_max) {
+			real_max = top;
 		}
+		
+		if (!check) {
+			reversed_nums.push_back({ top,max });
+		}
+		else if (real_max_check) {
+			reversed_nums.push_back({ top,real_max });
+			real_max_check = false;
+		}
+		else {
+			reversed_nums.push_back({ top,top });
+		}
+		
+		if (!st.empty()) {
+			if (st.top() < top) {
+				check = false;
+				max = top;		
+			}
+			else if(st.top()>max) {
+				if (st.top() < real_max) {
+					real_max_check = true;
+				}
+				check = true;			
+			}
+		}
+	
 	}
-	cout << cnt;
+
+
+	for (auto it = reversed_nums.rbegin(); it != reversed_nums.rend(); it++) {
+		if (it->first == it->second) {
+			cout << -1;
+		}
+		else {
+			cout << it->second;
+		}
+		if(it!=reversed_nums.rend())
+		cout << ' ';
+	}
 
 }
 
