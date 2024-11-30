03f7a9da0c2890c11975067a0c511353dd13ae07
royjwseo <royjwseo@gmail.com>
2024-11-30 17:01:08 +0900

    7785번 회사에 있는 사람 set사용

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -10,13 +10,14 @@
 #include <set>
 #include <unordered_set>
 #include <unordered_map>
+#include <map>
 #include <queue>
 #include <list>
 using namespace std;
 
 /*
 ================= 2024-11-28================
-10845번 큐
+7785번 회사에 있는 사람
 
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
@@ -28,48 +29,26 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
+	set<string> set;
+
 	int N;
 	cin >> N;
-	queue<int> q;
 	while (N--) {
-		string input;
-		cin >> input;
-		if (input == "push") {
-			int num;
-			cin >> num;
-			q.push(num);
-		}
-		else if (input == "pop") {
-			if (!q.empty()) {
-				cout << q.front() << '\n';
-				q.pop();
-			}
-			else
-				cout << -1 << '\n';
-		}
-		else if (input == "size") {
-			cout << q.size() << '\n';
-		}
-		else if (input == "empty") {
-			(q.empty()) ? cout << "1\n" : cout << "0\n";
-		}
-		else if (input == "front") {
-			if (q.empty()) {
-				cout << "-1\n";
-			}
-			else {
-				cout << q.front() << '\n';
-			}
+		string name, input;
+		cin >> name >> input;
+		if (input == "enter") {
+			set.insert(name);
 		}
 		else {
-			if (q.empty()) {
-				cout << "-1\n";
-			}
-			else
-			{
-				cout << q.back() << '\n';
-			}
+			set.erase(name);
 		}
+	}
+	
+	vector<string> names(set.begin(), set.end());
+
+	sort(names.begin(), names.end(), greater<string>());
+	for (auto a : names) {
+		cout << a << '\n';
 	}
 
 }
