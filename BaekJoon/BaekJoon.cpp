5ca078923593c91144ca6a99eff007a05023398f
royjwseo <royjwseo@gmail.com>
2024-12-02 19:20:51 +0900

    11399 atm greedy

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include <string>
-#include <sstream>
+#include <numeric>
 #include <array>
 #include <deque>
 #include <vector>
@@ -17,7 +17,7 @@ using namespace std;
 
 /*
 ================= 2024-12-02================
-[그리디] 2839번 설탕배달
+[그리디] 11399번 ATM
 
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
@@ -31,35 +31,18 @@ int main() {
 
 	int N;
 	cin >> N;
-	//5 . 3 
 
-	int fives = N / 5;
-	int fives_leftover = N % 5;
-	
+	vector<int> times(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> times[i];
+	}
+	sort(times.begin(), times.end());
+	int total{};
 	int result{};
-	bool find_answer = true;
-	for (int i = fives; i >= 0; --i) {
-		int cur_five = 5 * i;
-		int leftover = N - cur_five;
-		if (leftover > 0) {
-			if (leftover % 3 == 0) {
-				find_answer = true;
-				cout << i + (leftover / 3);
-				break;
-			}
-			else {
-				find_answer = false;
-			}
-		}
-		else {
-			find_answer = true;
-			cout << fives;
-			break;
-		}	
+	for (int i = 0; i < N; ++i) {
+		total = (total + times[i]);
+		result += total;
 	}
-	if (!find_answer) {
-		cout << -1;
-	}
-	
+	cout << result;
 }
 
