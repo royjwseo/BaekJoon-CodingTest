97479329d58c7c8a115999b8db70bff7357f390d
royjwseo <royjwseo@gmail.com>
2024-12-03 16:17:22 +0900

    [정수론] 1929번 소수 구하기 (에라토스테네스의 체)

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 #include <numeric>
+#include <sstream>
 #include <array>
 #include <deque>
 #include <vector>
@@ -16,40 +17,53 @@
 using namespace std;
 
 /*
-================= 2024-12-02================
-[그리디] 1931번 회의실 배정
+================= 2024-12-03================
+[정수론] 1929번 소수 구하기
 
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
+bool isPrime(int a) {
+	if (a % 2==0||a<=1)return false;
+	if (a ==2)return true;
+	int sqrtA = static_cast<int>(sqrt(a));
+	for (int i = 3; i <= sqrtA; i+=2) {
+		if (a % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void Eratosthenes(int start, int end) {
+	vector<bool> isPrime(end + 1,true);
+	isPrime[0] = false;
+	isPrime[1] = false;
+
+
+	for (int i = 2; i*i <= end; ++i) {
+		if (isPrime[i]) {
+			for (int j = i * i; j <= end; j += i) {
+				isPrime[j] = false;
+			}
+		}
+	}
+
+	for (int i = start; i <= end; ++i) {
+		if (isPrime[i])cout << i << '\n';
+	}
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N;
-	cin >> N;
+	int M, N;
+	cin >> M >> N;
 
-	vector<pair<int, int>>conferences(N);
-	for (int i = 0; i < N; ++i) {
-		cin >> conferences[i].first >> conferences[i].second;
-	}
-	sort(conferences.begin(), conferences.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-		return a.second < b.second;
-		});
-	int cur_end = -1;
-	int cnt{};
-	pair<int, int> first_time=conferences[0];
-	for (int i = 1; i < N; ++i) {
-		if (conferences[i].first >= first_time.second) {
-			cnt++;
-			first_time = conferences[i];
-		}
-	}
-	cout << cnt;
+	Eratosthenes(M, N);
 
-	
 }
 
