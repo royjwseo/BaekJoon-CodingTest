c727bb47aa033e7280a7070edbe84df92ac42f48
royjwseo <royjwseo@gmail.com>
2024-12-03 16:37:55 +0900

    [정수론] 1934 최소공배수

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,33 +18,30 @@ using namespace std;
 
 /*
 ================= 2024-12-03================
-[정수론] 2609번 최대공약수와 최소공배수
-유클리드 호제법
+[정수론] 1934 최소공배수
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
-int gcd(int a,int b) {
 
-	while (b != 0) {
-		int temp = b;
-		b = a % b;
-		a = temp;
-	}
-	return a;
+int gcd(int a, int b) {
+
+	if (a%b==0)return b;
+
+	return gcd(b, a % b);
 }
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-
-	int A, B;
-	cin >> A >> B;
-
-	int gcd_result = gcd(A, B);
-	cout << gcd_result <<'\n';
-	cout << A * B / gcd_result;
+	
+	int T;
+	cin >> T;
+	while (T--) {
+		int a, b;
+		cin >> a >> b;
+		cout << a * b / gcd(a, b)<<'\n';
+	}
 }
 
