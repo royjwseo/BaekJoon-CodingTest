1e10cb6a0d0fd6b7fd9f7cc4176dd2da1cacf18e
royjwseo <royjwseo@gmail.com>
2024-12-03 18:08:19 +0900

    7576번 토마토 BFS활용 최적화 완벽X

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,52 +18,89 @@ using namespace std;
 
 /*
 ================= 2024-12-03================
-[정수론] 9020 골드바흐의 추측
+7576번 토마토
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
-vector<bool> isPrime; 
 
+vector<vector<int>>board;
+vector<vector<bool>>visited;
 
-void PrintPrimeAdds(int n) {
+int dirX[4] = { 1,0,0,-1 };
+int dirY[4] = { 0,1,-1,0 };
 
-	
-	int left{}, right{};
-	for (int i = 2; i <= n/2; ++i) {
-		if (isPrime[i]&&isPrime[n-i]) {
-			left = i;
-			right = n - i;
-		}
+int BFS(vector<pair<int,int>> start,int max_x,int max_y) {
+	queue<pair<int,int>> q;
+	int cnt{};
+	int result{};
+	int once = start.size();
+	int newtomato{};
+	for (int i = 0; i < once; ++i) {
+		q.push(start[i]);
+		visited[start[i].first][start[i].second] = true;
 	}
-	cout << left << ' ' << right;
-}
+	while (!q.empty()) {
+		pair<int, int> cur = q.front();
+		q.pop();
+		cnt++;
+		if (cnt > once) {
+			result++;
+			once += newtomato;
+			newtomato = 0;
+		}
+		for (int i = 0; i < 4; ++i) {
+			int dx = cur.second + dirX[i];
+			int dy = cur.first + dirY[i];
+			if (dx < max_x && dx >= 0 && dy < max_y && dy >= 0) {
+				if (!visited[dy][dx]) {
+					if (board[dy][dx] == -1) {
+						visited[dy][dx] = true;
+					}
+					else if (board[dy][dx] == 0) {
+						visited[dy][dx] = true;
+						board[dy][dx] = 1;
+						q.push({ dy, dx });
+						newtomato++;
+					}
+				}
+			}
+		}
 
+	}
+	return result;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
-	
-	isPrime.resize(10001, true);
-	isPrime[0] = false;
-	isPrime[1] = false;
-	for (int i = 2; i * i <= 10000; ++i) {
-		if (isPrime[i]) {
-			for (int j = i*i; j <= 10000; j += i) {
-				isPrime[j] = false;
+	int x, y;
+	cin >> x >> y;
+	board.resize(y, vector<int>(x));
+	visited.resize(y, vector<bool>(x, false));
+	vector<pair<int, int>> start;
+	for (int i = 0; i < y; ++i) {
+		for (int j = 0; j < x; ++j) {
+			cin >> board[i][j];
+			if (board[i][j] == 1) {
+				start.push_back({ i,j });
 			}
 		}
 	}
-
-
-	int T;
-	cin >> T;
-	while (T--) {
-		int n;
-		cin >> n;
-
-		PrintPrimeAdds(n);
+	int res = BFS(start,x,y);
+	bool isAll = true;
+	for (int i = 0; i < y; ++i) {
+		if (count(board[i].begin(), board[i].end(), 0)>0) {
+			isAll = false;
+			break;
+		}	
+	}
+	if (isAll) {
+		cout << res;
+	}
+	else {
+		cout << -1;
 	}
 	
 }
