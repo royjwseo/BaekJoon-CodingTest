afd4861925a0bee72c479b15c3200accb1fb60a9
royjwseo <royjwseo@gmail.com>
2024-12-07 19:19:15 +0900

    [이분탐색-Parametric Search] 2110번 공유기 설치

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 /*
 ================= 2024-12-07================
-[이진 탐색] 1654번 랜선 자르기
+[이진 탐색] 2110번 공유기 설치
 
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
@@ -27,38 +27,41 @@ using namespace std;
 
 
 
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	
-	int K, N;
-	cin >> K >> N;
-	vector<int> lansons(K);
-	for (int i = 0; i < K; ++i)cin >> lansons[i];
+	using ll = long long;
+	int N,C;
+	cin >> N >> C;
 
-	sort(lansons.begin(), lansons.end());
+	vector<ll>vec(N);
+	for (int i = 0; i < N; ++i)cin >> vec[i];
 
-	long long min = 0;
-	long long max = lansons[K - 1];
+	sort(vec.begin(), vec.end());
+
+	ll min =1;
+	ll max = vec[N - 1]-vec[0];
 	int result{};
 	while (min <= max) {
-		long long mid = (min + max) / 2;
-		long long sum{};
-
-		for (int a : lansons) {
-			if (a > mid) {
-				sum += a / mid;
+		ll mid = (min + max) / 2;
+		ll last_pos = vec[0];
+		int cnt=1;
+		for (int i = 1; i < N; ++i) {
+			if (vec[i] - last_pos >= mid) {
+				cnt++;
+				last_pos = vec[i];
 			}
 		}
-		if (sum >= N) {
+		if (cnt >= C) {
 			result = mid;
-			min = mid+1;
+			min = mid + 1;
 		}
 		else {
 			max = mid - 1;
 		}
-	}
 
+	}
 	cout << result;
 }
 
