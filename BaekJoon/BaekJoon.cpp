5f64183833b6378800aee36cf39f11e99af47c16
royjwseo <royjwseo@gmail.com>
2024-12-14 17:30:30 +0900

    [코테 강의 1주차 문자열,누적합,구현] 10808 알파벳 개수

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 /*
 ================= 2024-12-14================
-[코테 강의 1주차 문자열,누적합,구현] 2309 일곱 난쟁이
+[코테 강의 1주차 문자열,누적합,구현] 10808 알파벳 개수
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -29,31 +29,16 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	vector<int>heights(9);
-	for (int i = 0; i < 9; ++i) {
-		cin >> heights[i];
-	}
-	sort(heights.begin(), heights.end());
-	int sum=accumulate(heights.begin(), heights.end(),0);
-	int left = 0;
-	int right = 1;
-	while (left<8) {
-		int two_sum = heights[left] + heights[right];
-		if (sum - two_sum == 100) {
-			break;
-		}
-		else {
-			right++;
-			if (right > 8) {
-				left++;
-				right = left + 1;
-			}
-		}
+	array<int, 26>alphabets{};
+	
+	string input;
+	cin >> input;
+	for (char a : input) {
+		alphabets[a - 'a']++;
 	}
 
-	for (int i = 0; i < 9; ++i) {
-		if(i!=left&&i!=right)
-		cout << heights[i] << '\n';
+	for (int a : alphabets) {
+		cout << a << ' ';
 	}
 }
 
