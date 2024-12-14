71d79538e80e522f7e658df21e69c8ddac2b9346
royjwseo <royjwseo@gmail.com>
2024-12-14 18:57:59 +0900

    [코테 강의 1주차 문자열,누적합,구현] 2559 수열

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 /*
 ================= 2024-12-14================
-[코테 강의 1주차 문자열,누적합,구현] 9996번 한국이 그리울 땐 서버에 접속하지
+[코테 강의 1주차 문자열,누적합,구현] 2559 수열
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -29,46 +29,26 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N;
-	cin >> N;
-	string pattern;
-	cin >> pattern;
+	int N, K;
+	cin >> N >> K;
 
-	string pat1{}, pat2{};
-	bool check = false;
-	for (int i = 0; i < pattern.size(); ++i) {
-		if (pattern[i] == '*') { check = true; continue; }
-		if (!check)pat1 += pattern[i];
-		else pat2 += pattern[i];
-	}
-	int pat1size = pat1.size();
-	int pat2size = pat2.size();
+	vector<int>added_nums(N + 1, 0);
 
-	for (int i = 0; i < N; ++i) {
-		bool isresult = true;
-		string input;
+	for (int i = 1; i <= N; ++i) {
+		int input;
 		cin >> input;
-		if (input.size() < pat1size + pat2size) {
-			cout << "NE\n";
-			continue;
-		}
-		for (int i = 0; i < pat1.size(); ++i) {
-			if (input[i] != pat1[i]) {
-				isresult = false;
-			}
-		}
-		for (int i = 0; i <pat2.size(); i++) {
-			if (input[input.size() - pat2.size() + i] != pat2[i]) {
-				isresult = false;
-			}
-		}
-		if (!isresult) {
-			cout << "NE\n";
-		}
-		else {
-			cout << "DA\n";
-		}
+		added_nums[i] = added_nums[i - 1] + input;
 	}
+
+	int max = numeric_limits<int>::min();
+	int left = 0;
+	while (left+K <= N) {
+		int sum = added_nums[left+K] - added_nums[left];
+		if (sum > max)max = sum;
+		left++;
+	}
+
+	cout << max;
 
 }
 
