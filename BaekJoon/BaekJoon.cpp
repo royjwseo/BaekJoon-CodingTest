2422d76def904c14af5d47d4b71d32b44b9fe738
royjwseo <royjwseo@gmail.com>
2024-12-16 18:36:36 +0900

    [코테 강의 2주차 그래프이론, BFS, DFS]  2583번 영역 구하기

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,84 +18,79 @@ using namespace std;
 
 /*
 ================= 2024-12-16================
-[코테 강의 2주차 그래프이론, BFS, DFS]  2468번 안전 영역
+[코테 강의 2주차 그래프이론, BFS, DFS]  2583번 영역 구하기 
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
+vector<vector<int>>field;
+vector<vector<bool>>visited;
+
+void fillField(pair<int, int> left, pair<int, int> right) {
+	for (int i = left.second; i < right.second; ++i) {
+		for (int j = left.first; j < right.first; ++j) {
+			visited[i][j] = true;
+		}
+	}
+}
+
 int dirX[4] = { 1,0,0,-1 };
 int dirY[4] = { 0,1,-1,0 };
 
-void DFS(int x,int y, vector<vector<bool>>&visited) {
+void DFS(int x, int y,int w,int h,int& cnt) {
 
-	if(!visited[y][x])
-	visited[y][x] = true;
+	if (!visited[y][x]) {
+		cnt++;
+		visited[y][x] = true;
+	}
 
 	for (int i = 0; i < 4; ++i) {
 		int dx = x + dirX[i];
 		int dy = y + dirY[i];
-		if (dx >= 0 && dx < visited[0].size() && dy >= 0 && dy < visited[0].size()) {
+		if (dx >= 0 && dx < w && dy >= 0 && dy < h) {
 			if (!visited[dy][dx]) {
-				DFS(dx, dy, visited);
+				DFS(dx, dy, w, h, cnt);
 			}
 		}
 	}
-
-
 }
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N;
-	cin >> N;
+	int Y, X, N;
+	cin >> Y >> X >> N;
 
-	vector<vector<int>>map(N, vector<int>(N, 0));
-	vector<vector<bool>>visited(N, vector<bool>(N, false));
-	int min = 101;
-	int max = 0;
+	field.resize(Y, vector<int>(X, 0));
+	visited.resize(Y, vector<bool>(X, false));
+
 	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
-			cin >> map[i][j];
-			if (map[i][j] < min) {
-				min = map[i][j];
-			}
-			if (map[i][j] > max) {
-				max = map[i][j];
-			}
-		}
+		pair<int, int> left, right;
+		cin >> left.first >> left.second >> right.first >> right.second;
+		fillField(left, right);
 	}
 
-	int result{};
-	for (int i = min; i <= max; ++i) {
-		int cnt{};
-		for(int u=0;u<N;++u)
-		fill(visited[u].begin(), visited[u].end(), false);
-		
-		for (int j = 0; j < N; ++j) {
-			for (int k = 0; k < N; ++k) {
-				if (map[j][k] < i) {
-					visited[j][k] = true;
-				}
+	vector<int> answer;
+	int cnt{};
+	for (int i = 0; i < Y; ++i) {
+		for (int j = 0; j < X; ++j) {
+			int size{};
+			if (!visited[i][j]) {
+				cnt++;
+				DFS(j, i, X, Y, size);
 			}
-		}
-
-		for (int j = 0; j < N; ++j) {
-			for (int k = 0; k < N; ++k) {
-				if (!visited[j][k]) {
-					cnt++;
-					DFS(k, j, visited);
-				}
-			}
-		}
-		if (cnt > result) {
-			result = cnt;
+			if (size > 0)answer.push_back(size);
 		}
 	}
-	cout << result;
+	sort(answer.begin(), answer.end());
+	cout << cnt << '\n';
+	for (int a : answer) {
+		cout << a << ' ';
+	}
+
+
 
 
 
