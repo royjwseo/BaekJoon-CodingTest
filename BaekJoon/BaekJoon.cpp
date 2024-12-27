fbdaea4c4f71695e51c27426583a3dfc46a69579
royjwseo <royjwseo@gmail.com>
2024-12-27 16:58:01 +0900

    [5주차 그리디,라인스위핑,투포인터] 9935 문자열 폭발

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 /*
 ================= 2024-12-24================
-[5주차 그리디,라인스위핑,투포인터] 2109번 순회강연
+[5주차 그리디,라인스위핑,투포인터] 9935 문자열 폭발
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -31,35 +31,25 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N;
-	cin >> N;
+	string input;
+	cin >> input;
+	string cmp;
+	cin >> cmp;
 
-	vector<pair<int, int>>conf(N);
-	for (int i = 0; i < N; ++i) {
-		cin >> conf[i].first >> conf[i].second;
-	}
-	sort(conf.begin(), conf.end(), [](const pair<int, int>& l, const pair<int, int>& r) {
-		if (l.second == r.second)return l.first > r.first;
-		return l.second < r.second;
-		});
-
-	priority_queue<int,vector<int>, greater<int>>pq;
-
-	for (auto& con : conf) {
-		pq.push(con.first);
-		if (pq.size() > con.second) {
-			pq.pop();
+	string result = "";
+	int cmp_len = cmp.length();
+	for (char a : input) {
+		result += a;
+		if (result.size()>=cmp_len&&result.substr(result.size() - cmp_len) == cmp) {
+			result.erase(result.size() - cmp_len);
 		}
 	}
-
-	int sum{};
-
-	while (!pq.empty()) {
-		sum += pq.top();
-		pq.pop();
+	if (!result.empty()) {
+		cout << result;
 	}
-	cout << sum;
-
+	else {
+		cout << "FRULA";
+	}
 	
 }
 
