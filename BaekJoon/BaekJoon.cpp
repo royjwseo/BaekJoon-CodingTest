32b3a2c58a139d3e16eeec3bba3159f00319b6c8
royjwseo <royjwseo@gmail.com>
2024-12-27 16:03:41 +0900

    [5주차 그리디,라인스위핑,투포인터] 2109번 순회강연

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 /*
 ================= 2024-12-24================
-[6주차 이분탐색 & LIS(최대증가부분수열)] 6236번 용돈 관리
+[5주차 그리디,라인스위핑,투포인터] 2109번 순회강연
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -31,47 +31,35 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N, M;
-	cin >> N >> M;
+	int N;
+	cin >> N;
 
-	vector<int>money(N);
-	int sum{};
-	int max_money{};
+	vector<pair<int, int>>conf(N);
 	for (int i = 0; i < N; ++i) {
-		cin >> money[i];
-		sum += money[i];
-		max_money = max(max_money, money[i]);
+		cin >> conf[i].first >> conf[i].second;
 	}
-	int left = max_money;
-	int right = sum;
-	int result{};
-	while (left <= right) {
-		int mid = (left + right) / 2;
+	sort(conf.begin(), conf.end(), [](const pair<int, int>& l, const pair<int, int>& r) {
+		if (l.second == r.second)return l.first > r.first;
+		return l.second < r.second;
+		});
 
-		int leftover = mid;
-		int cnt{};
+	priority_queue<int,vector<int>, greater<int>>pq;
+
+	for (auto& con : conf) {
+		pq.push(con.first);
+		if (pq.size() > con.second) {
+			pq.pop();
+		}
+	}
+
+	int sum{};
+
+	while (!pq.empty()) {
+		sum += pq.top();
+		pq.pop();
+	}
+	cout << sum;
+
 	
-		for (int i = 0; i < N; ++i) {
-			if (leftover - money[i] < 0) {
-				cnt++;
-				leftover = mid - money[i];
-			}
-			else {
-				leftover -= money[i];
-			}
-		}
-		if (leftover > 0)cnt++;
-
-		if (cnt > M) {
-			left = mid + 1;
-		}
-		else {
-			right = mid - 1;
-			result = mid;
-		}
-
-	}
-	cout << result;
-
 }
 
