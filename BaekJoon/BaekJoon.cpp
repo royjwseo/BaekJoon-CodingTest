e2d9cead67459831ee3c2ceab1064a8f1dc867e8
royjwseo <royjwseo@gmail.com>
2024-12-29 04:26:20 +0900

    [2주차그래프이론,DFS, BFS] 14502 연구소
    브루트포스로 DFS활용 및 조합(next_permutation or 3중반복문으로 경우의수구하기)

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,50 +18,81 @@ using namespace std;
 
 /*
 ================= 2024-12-29================
-[2주차그래프이론,DFS, BFS] 9012 괄호
+[2주차그래프이론,DFS, BFS] 14502 연구소
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
 
+vector<vector<int>>board;
+vector<vector<int>>copyboard;
+vector<pair<int,int>>coords;
+vector<pair<int,int>>virus;
+int N, M;
+int dirX[4] = { 1,0,0,-1 };
+int dirY[4] = { 0,1,-1,0 };
+
+void DFS(int y, int x) {
+
+	for (int i = 0; i < 4; ++i) {
+		int dx = x + dirX[i];
+		int dy = y + dirY[i];
+		if (dx >= 0 && dx < M && dy >= 0 && dy < N) {
+			if (board[dy][dx] == 0) {
+				board[dy][dx] = 2;
+				DFS(dy, dx);
+			}
+		}
+	}
+}
+
+int count() {
+	int cnt{};
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+			if (board[i][j] == 0)cnt++;
+		}
+	}
+	return cnt;
+}
+
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
+
+	cin >> N >> M;
+
+	board.resize(N, vector<int>(M,-1));
 	
-
-	int N;
-	cin >> N;
-
-	while (N--) {
-		stack<char> st;
-		string input;
-		cin >> input;
-
-		bool isAns = true;
-		for (char a : input) {
-			if (a == '(') {
-				st.push(a);
-			}
-			else if(a==')') {
-				if (!st.empty()) {
-					st.pop();
-				}
-				else {
-					isAns = false;
-					break;
-				}
-			}
-		}
-		if (!st.empty())isAns = false;
-	
-		if (isAns) {
-			cout << "YES\n";
-		}
-		else {
-			cout << "NO\n";
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+			cin >> board[i][j];
+			if (board[i][j] == 0)coords.push_back({ i,j });
+			if (board[i][j] == 2)virus.push_back({ i,j });
 		}
 	}
+	copyboard = board;
+	int ans{};
+	for (int i = 0; i < coords.size(); ++i) {
+		for (int j = i+1; j < coords.size(); ++j) {
+			for (int k = j+1; k < coords.size(); ++k) {
+				board[coords[i].first][coords[i].second] = 1;
+				board[coords[j].first][coords[j].second] = 1;
+				board[coords[k].first][coords[k].second] = 1;
+				for (auto a : virus) {
+					DFS(a.first, a.second);					
+				}
+				ans = max(ans, count());
+				board = copyboard;
+			}
+		}
+	}
+	cout << ans;
+	
+
+
 }
 
