3ebee29a6175f1415e04f182879c3d029e8815b0
royjwseo <royjwseo@gmail.com>
2024-12-29 01:34:16 +0900

    [2주차그래프이론,DFS, BFS] 2870번 수학숙제

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,63 +18,56 @@ using namespace std;
 
 /*
 ================= 2024-12-28================
-[2주차그래프이론,DFS, BFS] 4659번 비밀번호 발음하기
+[2주차그래프이론,DFS, BFS] 2870번 수학숙제
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
-bool checkIf(char a) {
-	if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')return true;
-	return false;
-}
+struct compare {
+
+	bool operator()(const string& a, const string& b) {
+		if (a.size() == b.size())return a < b;
+		else return a.size() < b.size();
+	}
+};
+
+
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
-	while (1) {
+	int N;
+	cin >> N;
+
+	vector<string>answer;
+	while (N--) {
 		string input;
 		cin >> input;
-		if (input == "end")break;
-		bool hasAnswer = true;
-		if (input.find('a') != string::npos || input.find('e') != string::npos
-			|| input.find('i') != string::npos || input.find('o') != string::npos
-			|| input.find('u') != string::npos) {
-
-			if (input.size() >= 2) {
-				char a = input[0];
-				for (int i = 1; i < input.size(); ++i) {
-					if (input[i] == a && input[i] !='e'&&input[i]!='o') {
-						cout << '<' << input << "> is not acceptable.\n";
-						hasAnswer = false;
-						continue;	
-					}
-					a = input[i];
+	
+		string result = "";
+		bool curAlpha = true;
+		for(int i=0;i!=input.size();++i){
+			if (isdigit(input[i])) {
+				if (result == "0")result = "";
+				result += input[i];
+				curAlpha = true;
+			}
+			else {
+				curAlpha = false;
+			}
+			if (i == input.size() - 1 || !curAlpha) {
+				if (!result.empty()) {
+					answer.push_back(result);
+					result = "";
 				}
 			}
-			if (input.size() >= 3) {
-				for (int i = 0; i < input.size() - 2; ++i) {
-					int cnt{};
-					string check = input.substr(i, 3);
-					for (int j = 0; j < check.size(); ++j) {
-						if (checkIf(check[j]))cnt++;
-						else cnt--;
-					}
-					if (cnt == 3 || cnt == -3) {
-						cout << '<' << input << "> is not acceptable.\n";
-						hasAnswer = false;
-
-						continue;
-					}
-				}
-			}
-			if(hasAnswer)
-			cout << '<' << input << "> is acceptable.\n";
 		}
-		else {
-			cout << '<' << input << "> is not acceptable.\n";
-		}
+	}
+	sort(answer.begin(), answer.end(),compare());
+	for (auto a : answer) {
+		cout << a << '\n';
 	}
 
 }
