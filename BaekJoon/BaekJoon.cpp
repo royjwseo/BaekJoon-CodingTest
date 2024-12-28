4e36e450db34e9f2591e45e791023910268d10e2
royjwseo <royjwseo@gmail.com>
2024-12-29 01:52:57 +0900

    [2주차그래프이론,DFS, BFS] 10709번 기상캐스터

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,56 +18,49 @@ using namespace std;
 
 /*
 ================= 2024-12-28================
-[2주차그래프이론,DFS, BFS] 2870번 수학숙제
+[2주차그래프이론,DFS, BFS] 10709번 기상캐스터
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
-struct compare {
-
-	bool operator()(const string& a, const string& b) {
-		if (a.size() == b.size())return a < b;
-		else return a.size() < b.size();
-	}
-};
 
 
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	
-	int N;
-	cin >> N;
 
-	vector<string>answer;
-	while (N--) {
-		string input;
-		cin >> input;
-	
-		string result = "";
-		bool curAlpha = true;
-		for(int i=0;i!=input.size();++i){
-			if (isdigit(input[i])) {
-				if (result == "0")result = "";
-				result += input[i];
-				curAlpha = true;
+	int H, W;
+	cin >> H >> W;
+
+	vector<vector<char>>map(H, vector<char>(W, 0));
+	for (int i = 0; i < H; ++i) {
+		for (int j = 0; j < W; ++j) {
+			cin >> map[i][j];
+		}
+	}
+	vector<vector<int>>answer(H, vector<int>(W, -1));
+
+	for(int j=0;j<W;++j){
+		for (int i = 0; i < H; ++i) {
+			if (map[i][j] == 'c') { answer[i][j] = 0; 
+				continue;
 			}
-			else {
-				curAlpha = false;
-			}
-			if (i == input.size() - 1 || !curAlpha) {
-				if (!result.empty()) {
-					answer.push_back(result);
-					result = "";
+			if (map[i][j] == '.'&&j==0)answer[i][j] = -1;
+
+			if (j > 0) {
+				if (map[i][j] == '.') {
+					if (answer[i][j - 1] >= 0)answer[i][j] = answer[i][j - 1] + 1;
 				}
 			}
 		}
 	}
-	sort(answer.begin(), answer.end(),compare());
-	for (auto a : answer) {
-		cout << a << '\n';
+
+	for (int i = 0; i < H; ++i) {
+		for (int j = 0; j < W; ++j) {
+			cout << answer[i][j] << ' ';
+		}cout << '\n';
 	}
 
 }
