3b47677d7df948ecba1d0daacd9451112e1071a0
royjwseo <royjwseo@gmail.com>
2024-12-31 16:48:53 +0900

    [2주차그래프이론,DFS, BFS] 1068 트리

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,84 +18,67 @@ using namespace std;
 
 /*
 ================= 2024-12-31================
-[2주차그래프이론,DFS, BFS] 2636 치즈
+[2주차그래프이론,DFS, BFS] 1068 트리
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
-vector<vector<int>>field;
-vector<vector<bool>>visited;
-vector<pair<int, int>>cheeze;
-int X, Y;
-int dirX[4] = { 1,0,0,-1 };
-int dirY[4] = { 0,-1,1,0 };
 
-void DFS(int y, int x) {
+vector<vector<int>>tree;
+vector<bool>visited;
 
-	visited[y][x] = true;
+int DFS(int here, int del_num) {
+	
+	int cnt{};
+	if (tree[here].size() > 1) {
+		for (int a : tree[here]) {
 
-	for (int i = 0; i < 4; ++i) {
-		int dx = x + dirX[i];
-		int dy = y + dirY[i];
-
-		if (dx >= 0 && dx < X && dy >= 0 && dy < Y) {
-			if (!visited[dy][dx]) {
-				visited[dy][dx] = true;
-				if (field[dy][dx] == 1) {
-					cheeze.push_back({ dy,dx });
-					field[dy][dx] = 0;
-				}
-				else {
-					DFS(dy, dx);
-				}
+			if (a == del_num) {
+				continue;
 			}
+			else {
+				cnt += DFS(a, del_num);
+			}
+
 		}
 	}
-}
-
-
-bool checkIfAll() {
-	for (int i = 0; i < Y; ++i) {
-		for (int j = 0; j < X; ++j) {
-			if (field[i][j] == 1) {
-				return false;
-			}
-		}
+	else {
+		return 1;
 	}
-	return true;
+	return cnt;
 }
+
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
+	int N;
+	cin >> N;
 
-	cin >> Y >> X;
+	tree.resize(N);
+	visited.resize(N, 0);
 
-	field.resize(Y, vector<int>(X, 0));
-	visited.resize(Y, vector<bool>(X, 0));
-
-	for (int i = 0; i < Y; ++i) {
-		for (int j = 0; j < X; ++j) {
-			cin >> field[i][j];
+	int root{};
+	for (int i = 0; i < N; ++i) {
+		int node;
+		cin >> node;
+		if (node == -1) {
+			root = i;
+			continue;
 		}
+		tree[node].push_back(i);
 	}
 
-	int cnt{};
-	while (1) {
 
-		for (auto& a : visited) {
-			fill(a.begin(), a.end(), false);
-		}
-		cheeze.clear();
-		DFS(0, 0);
-		cnt++;
-		if (checkIfAll()) {
-			cout << cnt << '\n';
-			cout << cheeze.size();
-			break;
-		}
+	int delete_node;
+	cin >> delete_node;
+	if (delete_node == root) {
+		cout << 0;
+	}
+	else {
+		cout << DFS(root, delete_node);
 	}
 }
 
