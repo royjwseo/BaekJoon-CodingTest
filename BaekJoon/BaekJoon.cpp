2be719919d4ad2ca2e4164bf88f8fe158139c3c1
royjwseo <royjwseo@gmail.com>
2024-12-31 18:51:10 +0900

    [2주차그래프이론,DFS, BFS] 17289 오큰수

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,27 +18,12 @@ using namespace std;
 
 /*
 ================= 2024-12-31================
-[2주차그래프이론,DFS, BFS] 1325 효율적인 해킹
+[2주차그래프이론,DFS, BFS] 17289 오큰수
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
-vector<vector<int>>adjacents;
-vector<int>visited;
-
-int DFS(int here) {
-	int cnt=1;
-	if (!visited[here]) visited[here] = true;
-	
-	
-		for (int a : adjacents[here]) {
-			if (!visited[a])
-				cnt += DFS(a);
-		}
-	
-	return cnt;
-}
 
 
 
@@ -46,37 +31,29 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N, M;
-	cin >> N >> M;
+	int N;
+	cin >> N;
 
-	adjacents.resize(N+1);
-	visited.resize(N + 1,0);
-	for (int i = 0; i < M; ++i) {
-		pair<int, int>input;
-		cin >> input.first >> input.second;
-		adjacents[input.second].push_back(input.first);
+	vector<int> nums(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> nums[i];
 	}
 
-	int max_res{};
-	vector<pair<int,int>>result;
-	for (int i = 1; i <= N; ++i) {
-		int cnt{};
-		fill(visited.begin(), visited.end(), 0);
-		cnt = DFS(i);
-		max_res = max(max_res, cnt);
-		result.push_back({cnt,i});
+	stack<int>st;
+	vector<int>answers(N,-1);
+
+	for (int i = 0; i < N; ++i) {
+
+		while(!st.empty()&&nums[st.top()] < nums[i]) {
+			answers[st.top()] = nums[i];
+			st.pop();
+		}
+		st.push(i);
 	}
 
-	sort(result.begin(), result.end(), [](const pair<int, int>& l, const pair<int, int>& r) {
-		if (l.first == r.first)return l.second < r.second;
-		return l.first > r.first;
-		});
-
-	for (auto a : result) {
-		if (a.first == max_res)cout << a.second << ' ';
+	for (auto a : answers) {
+		cout << a << ' ';
 	}
-
-
-
+	
 }
 
