b71f39bda46ea36431e5cb1a76b3310aed3d2b2d
royjwseo <royjwseo@gmail.com>
2024-12-31 15:56:15 +0900

    [2주차그래프이론,DFS, BFS] 2636 치즈

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -17,82 +17,85 @@
 using namespace std;
 
 /*
-================= 2024-12-29================
-[2주차그래프이론,DFS, BFS] 14502 연구소
+================= 2024-12-31================
+[2주차그래프이론,DFS, BFS] 2636 치즈
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
-
-vector<vector<int>>board;
-vector<vector<int>>copyboard;
-vector<pair<int,int>>coords;
-vector<pair<int,int>>virus;
-int N, M;
+vector<vector<int>>field;
+vector<vector<bool>>visited;
+vector<pair<int, int>>cheeze;
+int X, Y;
 int dirX[4] = { 1,0,0,-1 };
-int dirY[4] = { 0,1,-1,0 };
+int dirY[4] = { 0,-1,1,0 };
 
 void DFS(int y, int x) {
+
+	visited[y][x] = true;
 
 	for (int i = 0; i < 4; ++i) {
 		int dx = x + dirX[i];
 		int dy = y + dirY[i];
-		if (dx >= 0 && dx < M && dy >= 0 && dy < N) {
-			if (board[dy][dx] == 0) {
-				board[dy][dx] = 2;
-				DFS(dy, dx);
+
+		if (dx >= 0 && dx < X && dy >= 0 && dy < Y) {
+			if (!visited[dy][dx]) {
+				visited[dy][dx] = true;
+				if (field[dy][dx] == 1) {
+					cheeze.push_back({ dy,dx });
+					field[dy][dx] = 0;
+				}
+				else {
+					DFS(dy, dx);
+				}
 			}
 		}
 	}
 }
 
-int count() {
-	int cnt{};
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) {
-			if (board[i][j] == 0)cnt++;
+
+bool checkIfAll() {
+	for (int i = 0; i < Y; ++i) {
+		for (int j = 0; j < X; ++j) {
+			if (field[i][j] == 1) {
+				return false;
+			}
 		}
 	}
-	return cnt;
+	return true;
 }
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
 
-	cin >> N >> M;
+	cin >> Y >> X;
 
-	board.resize(N, vector<int>(M,-1));
-	
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) {
-			cin >> board[i][j];
-			if (board[i][j] == 0)coords.push_back({ i,j });
-			if (board[i][j] == 2)virus.push_back({ i,j });
+	field.resize(Y, vector<int>(X, 0));
+	visited.resize(Y, vector<bool>(X, 0));
+
+	for (int i = 0; i < Y; ++i) {
+		for (int j = 0; j < X; ++j) {
+			cin >> field[i][j];
 		}
 	}
-	copyboard = board;
-	int ans{};
-	for (int i = 0; i < coords.size(); ++i) {
-		for (int j = i+1; j < coords.size(); ++j) {
-			for (int k = j+1; k < coords.size(); ++k) {
-				board[coords[i].first][coords[i].second] = 1;
-				board[coords[j].first][coords[j].second] = 1;
-				board[coords[k].first][coords[k].second] = 1;
-				for (auto a : virus) {
-					DFS(a.first, a.second);					
-				}
-				ans = max(ans, count());
-				board = copyboard;
-			}
+
+	int cnt{};
+	while (1) {
+
+		for (auto& a : visited) {
+			fill(a.begin(), a.end(), false);
+		}
+		cheeze.clear();
+		DFS(0, 0);
+		cnt++;
+		if (checkIfAll()) {
+			cout << cnt << '\n';
+			cout << cheeze.size();
+			break;
 		}
 	}
-	cout << ans;
-	
-
-
 }
 
