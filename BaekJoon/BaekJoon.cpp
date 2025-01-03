fc8ac37e66d12ab46138fcb7f9d01b7ec4f746dd
royjwseo <royjwseo@gmail.com>
2025-01-03 16:35:50 +0900

    [Dijikstra] 1504번 특정한 최단 경로

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -17,68 +17,80 @@
 using namespace std;
 
 /*
-================= 2025-01-02================
-[Dijikstra] 1238번 파티
+================= 2025-01-03================
+[Dijikstra] 1504번 특정한 최단 경로
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
-vector<vector<pair<int, int>>>roads;
-vector<int>spent_time;
-vector<vector<int>>timelist;
 
-void Dijikstra(int start) {
-	
+vector<vector<pair<int, int>>>adjacents;
+vector<int>parent;
+
+vector<int> Dijikstra(int start,int num) {
+
+	vector<int> dist(num + 1, numeric_limits<int>::max());
+	dist[start] = 0;
 	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
-	spent_time[start] = 0;
 	pq.push({ 0,start });
+	parent[start] = start;
 
 	while (!pq.empty()) {
-		int cur_vill = pq.top().second;
-		int cur_spent = pq.top().first;
-
+		int curNode = pq.top().second;
+		int curCost = pq.top().first;
 		pq.pop();
-		if (cur_spent > spent_time[cur_vill])continue;
+		if (curCost > dist[curNode])continue;
 
-		for (auto& next : roads[cur_vill]) {
-			int next_vill = next.first;
-			int next_spent = next.second;
-			if (spent_time[cur_vill] + next_spent < spent_time[next_vill]) {
-				spent_time[next_vill] = spent_time[cur_vill] + next_spent;
-				pq.push({ spent_time[next_vill],next_vill });
+		for (auto& next : adjacents[curNode]) {
+			int nextNode = next.first;
+			int nextCost = next.second;
+			if (dist[curNode] + nextCost < dist[nextNode]) {
+				dist[nextNode] = dist[curNode] + nextCost;
+				parent[nextNode] = curNode;
+				pq.push({ dist[nextNode],nextNode });
 			}
 		}
 	}
+	return dist;
 }
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N, X, M;
-	cin >> N >> M >> X;
-	roads.resize(N + 1);
-	spent_time.resize(N + 1, numeric_limits<int>::max());
-	timelist.resize(N + 1);
-	for (int i = 0; i < M; ++i) {
-		int from, to, amount;
-		cin >> from >> to >> amount;
-		roads[from].push_back({ to,amount });
-	}
+	int N, E;
+	cin >> N >> E;
 
-	for (int i = 1; i <= N; ++i) {
-		Dijikstra(i);
-		timelist[i] = spent_time;
-		fill(spent_time.begin(), spent_time.end(), numeric_limits<int>::max());
+	adjacents.resize(N+1);
+	parent.resize(N + 1);
+
+	for (int i = 0; i < E; ++i) {
+		int from, to, weight;
+		cin >> from >> to >> weight;
+		adjacents[from].push_back({ to,weight });
+		adjacents[to].push_back({ from,weight });
 	}
 	
-	int max_res{};
-	for (int i = 1; i <= N; ++i) {
-		int res = timelist[X][i] + timelist[i][X];
-		max_res = max(max_res, res);
+	int pass_from, pass_to;
+	cin >> pass_from >> pass_to;
+
+	vector<int> FullDist=Dijikstra(1,N);
+	vector<int> v1v2 = Dijikstra(pass_from, N);
+	vector<int> v2v1 = Dijikstra(pass_to, N);
+
+	long long v1v2Dist = FullDist[pass_from] + v1v2[pass_to] + v2v1[N];
+	long long v2v1Dist = FullDist[pass_to] + v2v1[pass_from] + v1v2[N];
+
+	long long minDist = min(v1v2Dist, v2v1Dist);
+
+	if (minDist >= numeric_limits<int>::max()) {
+		cout << -1;
 	}
-	cout << max_res;
+	else {
+		cout << minDist;
+	}
+
+
 }
 
