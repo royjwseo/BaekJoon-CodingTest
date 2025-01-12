3aca141fc50661f8f93ab453f5935524aadac9db
royjwseo <royjwseo@gmail.com>
2025-01-12 16:55:20 +0900

    21937번 작업

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,81 +18,50 @@
 using namespace std;
 
 /*
-================= 2025-01-10================
-14938번 서강그라운드
+================= 2025-01-12================
+21937번 작업
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
-int N, M, R;
-vector<int>items;
-vector<vector<pair<int, int>>>adjacents;
-vector<int>dist;
+vector<vector<int>>adjacents;
+vector<int>visited;
 
-void Dijikstra(int start) {
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
-	pq.push({ 0,start });
-	dist[start] = 0;
+int DFS(int here) {
 
-	while (!pq.empty()) {
-		int curNode = pq.top().second;
-		int curDist = pq.top().first;
-		pq.pop();
-
-		if (curDist > dist[curNode])continue;
-
-		for (int i = 0; i < adjacents[curNode].size(); ++i) {
-			int nextNode = adjacents[curNode][i].first;
-			int nextCost = adjacents[curNode][i].second;
-
-			if (curDist + nextCost < dist[nextNode]) {
-				dist[nextNode] = curDist + nextCost;
-				pq.push({ dist[nextNode],nextNode });
-			}
+	int cnt{};
+	if(!visited[here])
+	visited[here] = true;
+	
+	for (int i = 0; i < adjacents[here].size(); ++i) {
+		if (!visited[adjacents[here][i]]) {
+			cnt++;
+			cnt += DFS(adjacents[here][i]);
 		}
 	}
-
-	
+	return cnt;
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
  
-	cin >> N >> M >> R;
-
-	items.assign(N + 1, 0);
+	int N, M;
+	cin >> N >> M;
 	adjacents.resize(N + 1);
-	dist.resize(N + 1,numeric_limits<int>::max());
+	visited.resize(N + 1);
 
-
-	for (int i = 0; i < N; ++i) {
-		int input;
-		cin >> input;
-		items[i + 1] = input;
+	for (int i = 0; i < M; ++i) {
+		int from, to;
+		cin >> from >> to;
+		adjacents[to].push_back(from);
 	}
-
-	for (int i = 0; i < R; ++i) {
-		int from, to, fee;
-		cin >> from >> to >> fee;
-		adjacents[from].push_back({ to,fee });
-		adjacents[to].push_back({ from,fee });
-	}
-
-	int ans = -1;
-
-	for (int i = 2; i <= N; ++i) {
-		int cnt{};
-		fill(dist.begin(), dist.end(), numeric_limits<int>::max());
-		Dijikstra(i);
-		for (int k = 1; k <= N; ++k) {
-			if (dist[k] <= M)cnt += items[k];
-		}
-		ans=max(ans, cnt);
 	
-	}
+	int find;
+	cin >> find;
 
+	int ans = DFS(find);
 	cout << ans;
 
 }
