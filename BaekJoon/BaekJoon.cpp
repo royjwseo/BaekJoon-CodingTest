fd2eab1abc5379fd316e04f652a1594f619d4ecc
royjwseo <royjwseo@gmail.com>
2025-01-18 22:38:03 +0900

    17413번 단어 뒤집기 2

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,41 +18,61 @@
 using namespace std;
 
 /*
-================= 2025-01-15================
-17218번 비밀번호 만들기
+================= 2025-01-18================
+17413번 단어 뒤집기 2
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
 
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
-	
-	
 
-	int N, M;
-	cin >> N >> M;
-	unordered_map<string, string>um;
-	string flush;
-	getline(cin,flush);
-	for (int i = 0; i < N; ++i) {
-		string input;
-		getline(cin, input);
-		int where = input.find(' ');
-		um[input.substr(0, where)] = input.substr(where + 1);
+	string input;
+	getline(cin, input);
+
+	bool put_in = true;
+	bool reverse_pop = false;
+	deque<char>dq;
+	string ans = "";
+	for (int i = 0; i < input.size(); ++i) {
+		if (input[i] == '<') {
+			put_in = false;
+		}
+		if (input[i] == '>') {
+			put_in = true;
+		}
+		if (!put_in||i==input.size()-1)
+		{
+			if (i == input.size() - 1)dq.push_back(input[i]);
+			while (!dq.empty()) {
+				ans += dq.back();
+				dq.pop_back();
+			}
+			if (i != input.size() - 1)
+			ans += input[i];
+		}
+		else {
+			if (input[i] == ' ') {
+				while (!dq.empty()) {
+					ans += dq.back();
+					dq.pop_back();
+				}
+				ans += ' ';
+			}
+			else {
+				if (input[i] == '>') {
+					ans += '>';
+					continue;
+				}
+				dq.push_back(input[i]);
+			}
+		}
+		
 	}
+	cout << ans;
 
-
-
-
-	for (int i = 0; i < M; ++i) {
-		string input;
-		cin >> input;
-		if(!um[input].empty())
-		cout << um[input]<<'\n';
-	}
 }
 
