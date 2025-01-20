16026a6f839e42880a954601a3aedd832ed0aee1
royjwseo <royjwseo@gmail.com>
2025-01-20 18:40:41 +0900

    [32068번 보물 찾기] KOI 2024 2차 초등부

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -19,8 +19,7 @@ using namespace std;
 
 /*
 ================= 2025-01-20================
-[2024 국민대학교 & 중앙대학교 연합 프로그래밍 경진대회 Open Contest]
-32752번 수열이에요?
+[32068번 보물 찾기] KOI 2024 2차 초등부
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -31,30 +30,22 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N, L, R;
-	cin >> N >> L >> R;
+	int T;
+	cin >> T;
 
-	vector<int>nums(N);
-	for (int i = 0; i < N; ++i) {
-		int input;
-		cin >> input;
-		nums[i] = input;
-	}
+	while (T--) {
+		int L, R, S;
+		cin >> L >> R >> S;
+		
+		int min = (abs(L-S) >= abs(R-S)) ? R-S : L-S;
 
-	sort(nums.begin()+L-1,nums.begin()+R);
-
-
-	int compare = nums[0];
-	bool is = true;
-	for (int i = 1; i < N; ++i) {
-		if (compare > nums[i]) {
-			is = false;
-			break;
+		if (min < 0) {
+			cout << abs(min) * 2 + 1 << '\n';
 		}
-		compare = nums[i];
+		else {
+			cout << min * 2<<'\n';
+		}
 	}
-	if (!is)cout << 0;
-	else cout << 1;
 
 
 }
