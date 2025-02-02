bcf35c0ef629f97a80976190ed178a85bee14524
royjwseo <royjwseo@gmail.com>
2025-02-02 18:31:34 +0900

    [PCCP 기출문제] 3번 / 충돌위험 찾기 -> 실패

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -18,62 +18,125 @@
 using namespace std;
 
 /*
-================= 2025-01-20================
-[이분 탐색(실패)] 1300번 K번째 수
+================= 2025-02-02================
+[PCCP 기출문제] 3번 / 충돌위험 찾기 -> 실패함
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
-/*
-행렬 원소 값의 정의:
-행렬𝐴[𝑖][𝑗]
-A[i][j]는 (𝑖+1)×(𝑗+1)
-(i+1)×(j+1)로 정의됩니다.
-예를 들어, 행렬의 원소
-𝐴[𝑖][𝑗]=(𝑖+1)×(𝑗+1)
-A[i][j]=(i+1)×(j+1)입니다.
-countLessOrEqual(n, x) 함수:
-countLessOrEqual(n, x)는 주어진 수 x 이하인 모든 행렬 원소의 개수를 구하는 함수입니다.
+int solution(vector<vector<int>> points, vector<vector<int>> routes) {
+	int answer = 0;
 
-각 행에서 x 이하인 원소 개수 구하는 공식
-각 행𝑖 i에서 x 이하인 원소의 개수는 다음과 같습니다:
-count_in_row(𝑖,𝑥)=min⁡(𝑥/𝑖,𝑛)
-count_in_row(i,x)=min(x/i,n)
-*/
+	int n = points.size();
 
-long long cnt(int n, long long x) {
-	long long count{};
-	for (int i = 1; i <= n; ++i) {
-		count += min(x / i, (long long)n);
+	int n_robots = routes.size();
+	vector<pair<int, int>>start(n_robots);
+	vector<pair<int, int>>dest(n_robots);
+	vector<pair<int, int>>robo_dist(n_robots);
+	vector<vector<int>>cur_robots(101, vector<int>(101, 0));
+	for (int i = 0; i < n_robots; ++i) {
+		start[i].first = points[routes[i][0] - 1][0];
+		start[i].second = points[routes[i][0] - 1][1];
+
+		cur_robots[start[i].first][start[i].second]++;
+
+		dest[i].first = points[routes[i][1] - 1][0];
+		dest[i].second = points[routes[i][1] - 1][1];
+
+		robo_dist[i] = { dest[i].first - start[i].first,	dest[i].second - start[i].second };
 	}
-	return count;
+	int cnt{};
+	vector<bool>allDone(n_robots, false);
+	while (1) {
+		bool isFinished = true;
+
+		for (int i = 1; i < 101; ++i) {
+			for (int j = 1; j < 101; ++j) {
+				if (cur_robots[i][j] > 1)cnt += 1;
+			}
+		}
+
+
+		for (int i = 0; i < n_robots; ++i) {
+			if (allDone[i] == false) {
+				isFinished = false;
+			}
+		}
+		if (isFinished) {
+			break;
+		}
+
+		for (int i = 0; i < n_robots; ++i) {
+			cur_robots[start[i].first][start[i].second]--;
+			if (abs(robo_dist[i].first) != 0) {
+				if (robo_dist[i].first > 0) {
+					start[i].first += 1;
+					robo_dist[i].first -= 1;
+				}
+				else {
+					start[i].first -= 1;
+					robo_dist[i].first += 1;
+				}
+				cur_robots[start[i].first][start[i].second]++;
+			}
+			else if (abs(robo_dist[i].second > 0)) {
+				if (robo_dist[i].second > 0) {
+					start[i].second += 1;
+					robo_dist[i].second -= 1;
+				}
+				else {
+					start[i].second -= 1;
+					robo_dist[i].second += 1;
+				}
+				cur_robots[start[i].first][start[i].second]++;
+			}
+			else {
+				allDone[i] = true;
+			}
+		}
+
+
+
+
+	}
+	answer = cnt;
+	return answer;
 }
+
+
+
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N;
-	cin >> N;
-	int K;
-	cin >> K;
-	long long left = 1;
-	long long right = (long long)(N * N);
-	long long ans;
+	//[[3, 2], [6, 4], [4, 7], [1, 4]],
+		//[[4, 2], [1, 3], [4, 2], [4, 3]]
+	
+	vector<vector<int>>p(4, vector<int>(2));
+	vector<vector<int>>r(4, vector<int>(2));
 
-	while (left <= right) {
-		long long mid = (left + right) / 2;
-		if (cnt(N, mid) >= K) {
-			ans = mid;
-			right = mid - 1;
-		}
-		else {
-			left = mid + 1;
-		}
-	}
-	cout << ans;
+	p[0][0] = 3;
+	p[0][1] = 2;
+	p[1][0] = 6;
+	p[1][1] = 4;
+	p[2][0] = 4;
+	p[2][1] = 7;
+	p[3][0] = 1;
+	p[3][1] = 4;
+	
+	r[0][0] = 4;
+	r[0][1] = 2;
+	r[1][0] = 1;
+	r[1][1] = 3;
+	r[2][0] = 4;
+	r[2][1] = 2;
+	r[3][0] = 4;
+	r[3][1] = 3;
+
+	cout<<solution(p, r);
+
+	
 }
-// 1 2 3
-// 2 4 6
-// 3 6 9
+
