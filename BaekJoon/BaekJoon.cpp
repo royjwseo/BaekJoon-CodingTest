6303479d1a73704baa59ad5d77635f441ae7688b
royjwseo <royjwseo@gmail.com>
2025-02-04 13:08:29 +0900

    [이분 탐색 복습] 2805번 나무 자르기

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -19,36 +19,56 @@ using namespace std;
 
 /*
 ================= 2025-02-04================
-[이분 탐색 복습] 10816번 숫자 카드2
+[이분 탐색 복습] 2805번 나무 자르기
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
+bool leftover(const vector<long long>& trees, long long val, long long get) {
+	long long leftover{};
+	for (auto a : trees) {
+		if (a - val >= 0)
+			leftover += a - val;
+	}
+	if (leftover >= get)return true;
+	else return false;
+}
+
+long long binary(const vector<long long>& trees, long long get) {
+	long long left = 0;
+	long long right = trees[trees.size() - 1];
+	
+	long long result{};
+	while (left <= right) {
+		long long mid= left + (right - left) / 2;
+
+		if (leftover(trees, mid, get)) {
+			left = mid + 1;
+			result = mid;
+		}
+		else {
+			right = mid - 1;
+		}
+	}
+	return result;
+}
 
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int N;
-	cin >> N;
+	int N, M;
+	cin >> N >> M;
 
-	vector<int>cards(N);
+	vector<long long>trees(N);
 	for (int i = 0; i < N; ++i) {
-		cin >> cards[i];
+		cin >> trees[i];
 	}
-	sort(cards.begin(), cards.end());
+	sort(trees.begin(), trees.end());
 
-	int M;
-	cin >> M;
-	for (int i = 0; i < M; ++i) {
-		int input;
-		cin >> input;
-		cout << upper_bound(cards.begin(), cards.end(), input) - lower_bound(cards.begin(), cards.end(), input)<<' ';
-	}
-
-	
+	cout << binary(trees, M);
 	
 }
 
