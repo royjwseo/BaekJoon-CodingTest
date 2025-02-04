2baa469e67ede5a64effcf7685df092818508b90
royjwseo <royjwseo@gmail.com>
2025-02-04 15:41:20 +0900

    [이분 탐색 복습] 2776번 암기왕

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 /*
 ================= 2025-02-04================
-[이분 탐색 복습] 7795번 먹을 것인가 먹힐 것인가
+[이분 탐색 복습] 2776번 암기왕 
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -37,21 +37,24 @@ int main() {
 
 	while (T--) {
 		int N, M;
-		cin >> N >> M;
+		cin >> N;
+		vector<int>note1(N);
+		for (int i = 0; i < N; ++i)cin >> note1[i];
+	
+		cin >> M;
+		vector<int>note2(M);
+		for (int i = 0; i < M; ++i)cin >> note2[i];
 
-		vector<int>A(N), B(M);
-		for (int i = 0; i < N; ++i)cin >> A[i];
-		for (int i = 0; i < M; ++i)cin >> B[i];
+		sort(note1.begin(), note1.end());
 
-		sort(B.begin(), B.end());
-
-		int cnt{};
-		for (int i = 0; i < N; ++i) {
-			auto iter = lower_bound(B.begin(), B.end(), A[i]);
-			cnt += iter - B.begin();
+		for (auto a : note2) {
+			if (upper_bound(note1.begin(), note1.end(), a) - lower_bound(note1.begin(), note1.end(), a)) {
+				cout << 1 << '\n';
+			}
+			else {
+				cout << 0 << '\n';
+			}
 		}
-		cout << cnt<<'\n';
 	}
-
 }
 
