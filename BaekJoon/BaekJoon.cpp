4e966b102e796b03e643ade038fb18f99b67d054
royjwseo <royjwseo@gmail.com>
2025-02-06 14:23:05 +0900

    [DP] 9148번 신나는 함수 실행

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -19,39 +19,37 @@ using namespace std;
 
 /*
 ================= 2025-02-06================
-[DP] 24416번 알고리즘 수업 - 피보나치 수 1
+[DP] 9148번 신나는 함수 실행
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
 */
 
-pair<int,int> fib(int n) {
-	int cnt{};
-	cnt++;
-	if (n == 1 || n == 2)return {1,cnt};
-	pair<int, int>fib1 = fib(n - 1);
-	pair<int, int>fib2 = fib(n - 2);
-	return {fib1.first + fib2.first,fib1.second+fib2.second};
+int dp[50][50][50];
+
+int w(int a, int b, int c) {
+	if (a <= 0 || b <= 0 || c <= 0)return 1;
+
+	if (a > 20 || b > 20 || c > 20)return w(20,20,20);
+
+	if (dp[a][b][c] != 0)return dp[a][b][c];
+
+	if (a < b && b < c)return dp[a][b][c]=w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+	else return dp[a][b][c]=w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+
 }
 
-pair<int,int> fibonacci(int n) {
-	vector<int>f(n+1);
-	int cnt{};
-	f[1] = 1;
-	f[2] = 1;
-	for (int i = 3; i <= n; ++i) {
-		f[i] = f[i - 1] + f[i - 2];
-		cnt++;
-	}
-	return { f[n],cnt };
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int n;
-	cin >> n;
-	cout << fib(n).second << ' ' << fibonacci(n).second;
+	while (1) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1)break;
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << '\n';
+	}
+
 }
 
