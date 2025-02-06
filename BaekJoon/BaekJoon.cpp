dfeb9266127860cd90c1f63799fbfa4e54fe5c8e
royjwseo <royjwseo@gmail.com>
2025-02-06 16:07:40 +0900

    [DP] 1149번 RGB거리

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 /*
 ================= 2025-02-06================
-[DP] 1912번 연속합
+[DP] 1149번 RGB거리
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -54,16 +54,24 @@ int main() {
 	int N;
 	cin >> N;
 
-	int cur;
-	cin >> cur;
-	int max_cur = cur;
-	for (int i = 1; i < N; ++i) {
-		int input;
-		cin >> input;
-		cur = max(cur + input, input);
-		max_cur = max(max_cur, cur);
+	vector<vector<int>>cost(N, vector<int>(3)),dp;
+	dp = cost;
+	for (int i = 0; i < N; ++i) {
+		cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
 	}
-	cout << max_cur;
+	dp[0][0] = cost[0][0];
+	dp[0][1] = cost[0][1];
+	dp[0][2] = cost[0][2];
+
+	for (int i = 1; i < N; ++i) {					   
+		dp[i][0]=min(dp[i-1][1],dp[i-1][2])+cost[i][0] ;
+		dp[i][1]=min(dp[i-1][0],dp[i-1][2])+cost[i][1] ;
+		dp[i][2]=min(dp[i-1][0],dp[i-1][1])+cost[i][2] ;
+	}
+	
+	cout << min({ dp[N - 1][0],dp[N - 1][1],dp[N - 1][2] });
+
+	
 }
 
 
