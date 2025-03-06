7467c9fdf067e8271a343b668bad59256ed8ca73
royjwseo <royjwseo@gmail.com>
2025-03-06 19:33:18 +0900

    2748번 피보나치 수 2

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 /*
 ================= 2025-03-06================
-11727번 2xn 타일링 2
+2748번 피보나치 수 2
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -32,23 +32,16 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int N;
-	cin >> N;
-
-	vector<int>dp(N+1);
+	int n;
+	cin >> n;
+	vector<long long>dp(n+1);
 	dp[0] = 0;
 	dp[1] = 1;
-	dp[2] = 3;//3
-	dp[3] = 5;//5 1+2+2+
-	dp[4] = 13;
-	dp[5] = 23;
-	for (int i = 3; i <= N; ++i) {
-		
-		dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
-
+	dp[2] = 1;
+	for (int i = 3; i <= n; ++i) {
+		dp[i] = dp[i - 1] + dp[i - 2];
 	}
-	cout << dp[N - 1];
-
+	cout << dp[n];
 
 }
 
