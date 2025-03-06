03ac7b5f6750dfcf0e23f943a09842b378862f63
royjwseo <royjwseo@gmail.com>
2025-03-06 19:22:44 +0900

    11727번 2xn 타일링 2

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 /*
 ================= 2025-03-06================
-11053번 가장 긴 증가하는 부분 수열
+11727번 2xn 타일링 2
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -35,22 +35,21 @@ int main() {
 	int N;
 	cin >> N;
 
-	vector<int>nums(N);
-	for (int i = 0; i < N; ++i) {
-		cin >> nums[i];
-	}
+	vector<int>dp(N+1);
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 3;//3
+	dp[3] = 5;//5 1+2+2+
+	dp[4] = 13;
+	dp[5] = 23;
+	for (int i = 3; i <= N; ++i) {
+		
+		dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
 
-	vector<int>dp(N, 1);
-	int last_index = 0;
-	for (int i = 1; i < N; ++i) {
-
-		for (int j = i; j >= 0; --j) {
-			if (nums[i] > nums[j]) {
-				dp[i] = max(dp[j] + 1,dp[i]);
-			}
-		}
 	}
-	cout << *max_element(dp.begin(), dp.end());
+	cout << dp[N - 1];
+
+
 }
 
 
