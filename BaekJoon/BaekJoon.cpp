615771f495c7368b8a16ddf35ded5c79c3018cd1
royjwseo <royjwseo@gmail.com>
2025-03-06 17:40:43 +0900

    1003번 피보나치 함수

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 /*
 ================= 2025-03-06================
-2839번 설탕 배달
+1003번 피보나치 함수
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -31,24 +31,33 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
-	//3kg 5kg 최대한 적은 봉지 
+	int T;
+	cin >> T;
 
-	int N;
-	cin >> N;
+	while (T--) {
+		int N;
+		cin >> N;
+		vector<vector<int>>dp(N+1,vector<int>(2));
+		if (N == 0) { cout << 1 << ' ' << 0 << '\n'; continue; }
+		if (N == 1) {
+			cout << 0 << ' ' << 1 << '\n'; continue;
+		}
 
-	int cnt{};
-	
-	while (N % 5 != 0) {
-		N -= 3;
-		if (N < 0)break;
-		cnt++;
+		if (N > 1) {
+			dp[0][0] = 1;
+			dp[0][1] = 0;
+			dp[1][0] = 0;
+			dp[1][1] = 1;
+
+			for (int i = 2; i <= N; ++i) {
+				dp[i][0] = dp[i - 2][0] + dp[i - 1][0];
+				dp[i][1] = dp[i - 2][1] + dp[i - 1][1];
+			}
+			cout << dp[N][0] << ' ' << dp[N][1] << '\n';
+		}
+
+		
 	}
-	if (N < 0)cout << -1;
-	else {
-		cnt += N / 5;
-		cout << cnt;
-	}
-
 
 }
 
