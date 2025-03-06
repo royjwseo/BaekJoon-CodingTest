fa49ec783251e45545dae67946e5ac061fbc4584
royjwseo <royjwseo@gmail.com>
2025-03-06 17:53:45 +0900

    11726번 2xn 타일링

--- BaekJoon/BaekJoon.cpp
+++ BaekJoon/BaekJoon.cpp
@@ -19,7 +19,7 @@ using namespace std;
 
 /*
 ================= 2025-03-06================
-1003번 피보나치 함수
+11726번 2xn 타일링
 1KB -> 1024바이트
 1MB -> 1000KB -> 1024 * 1024 바이트 대략 262'144개 int저장가능
 스택 크기 : 1MB
@@ -31,33 +31,17 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	
-	int T;
-	cin >> T;
+	int N;
+	cin >> N;
 
-	while (T--) {
-		int N;
-		cin >> N;
-		vector<vector<int>>dp(N+1,vector<int>(2));
-		if (N == 0) { cout << 1 << ' ' << 0 << '\n'; continue; }
-		if (N == 1) {
-			cout << 0 << ' ' << 1 << '\n'; continue;
-		}
-
-		if (N > 1) {
-			dp[0][0] = 1;
-			dp[0][1] = 0;
-			dp[1][0] = 0;
-			dp[1][1] = 1;
-
-			for (int i = 2; i <= N; ++i) {
-				dp[i][0] = dp[i - 2][0] + dp[i - 1][0];
-				dp[i][1] = dp[i - 2][1] + dp[i - 1][1];
-			}
-			cout << dp[N][0] << ' ' << dp[N][1] << '\n';
-		}
-
-		
+	vector<long long>dp(N+1);
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= N; ++i) {
+		dp[i] = (dp[i - 1] + dp[i - 2])%10007;
 	}
+	cout << dp[N];
 
 }
 
